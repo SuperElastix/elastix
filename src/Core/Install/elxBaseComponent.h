b0c82bae279a8d41c62af728387978eb56526e3a
Marius Staring <m.staring@lumc.nl>
2005-06-07 18:08:09 +0000

    ---------------------------------------------------------------------- MS:
    - Increased elastix version to 3.106
    ----------------------------------------------------------------------

--- src/Core/Install/elxBaseComponent.h
+++ src/Core/Install/elxBaseComponent.h
@@ -21,7 +21,7 @@
 
 #include <iostream>
 /** The current Elastix version. */
-#define __ELASTIX_VERSION 3.105
+#define __ELASTIX_VERSION 3.106
 
 /** All Elastix components should be in namespace elastix. */
 namespace elastix
