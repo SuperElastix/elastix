4cbd30118efd0d7051dbbd385b9d20625c712d93
Stefan Klein <s.klein@erasmusmc.nl>
2006-05-23 15:12:55 +0000

    SK:
    - version -> 3.504

--- src/Core/Install/elxBaseComponent.h
+++ src/Core/Install/elxBaseComponent.h
@@ -22,7 +22,7 @@
 #include <iostream>
 
 /** The current Elastix version. */
-#define __ELASTIX_VERSION 3.503
+#define __ELASTIX_VERSION 3.504
 
 /** All Elastix components should be in namespace elastix. */
 namespace elastix
