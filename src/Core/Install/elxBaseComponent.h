0a43ca75ce2114bb3c719c043174c7a2b1ef721e
Marius Staring <m.staring@lumc.nl>
2006-08-02 12:34:10 +0000

    MS:
    ENH: increased elastix version to 3.509

--- src/Core/Install/elxBaseComponent.h
+++ src/Core/Install/elxBaseComponent.h
@@ -22,7 +22,7 @@
 #include <iostream>
 
 /** The current Elastix version. */
-#define __ELASTIX_VERSION 3.508
+#define __ELASTIX_VERSION 3.509
 
 /** All Elastix components should be in namespace elastix. */
 namespace elastix
