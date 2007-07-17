6cd00a34679946c231038f59b6a2e5729027a6bf
Marius Staring <m.staring@lumc.nl>
2007-07-17 15:34:20 +0000

    MS:
    - ENH: increased elastix version to 3.706

--- src/Core/Install/elxBaseComponent.h
+++ src/Core/Install/elxBaseComponent.h
@@ -23,7 +23,7 @@
 #include <sstream>
 
 /** The current Elastix version. */
-#define __ELASTIX_VERSION 3.705
+#define __ELASTIX_VERSION 3.706
 
 /** All Elastix components should be in namespace elastix. */
 namespace elastix
