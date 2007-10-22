9bc29b110df0762c4638e7eb3fa1f4d3bf4810df
Marius Staring <m.staring@lumc.nl>
2007-10-22 18:05:36 +0000

    MS:
    - ENH: increased the elastix version number to 3.712, since we just added the progress feature and solved a major bug

--- src/Core/Install/elxBaseComponent.h
+++ src/Core/Install/elxBaseComponent.h
@@ -23,7 +23,7 @@
 #include <sstream>
 
 /** The current Elastix version. */
-#define __ELASTIX_VERSION 3.711
+#define __ELASTIX_VERSION 3.712
 
 /** All Elastix components should be in namespace elastix. */
 namespace elastix
