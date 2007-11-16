863e3e45991d6acac4b33100ec09983a36c25fc7
Marius Staring <m.staring@lumc.nl>
2007-11-16 16:34:21 +0000

    MS:
    - ENH: implemented UpdateFixedMasks and UpdateMovingMasks. Assuming only one mask.

--- src/Components/Registrations/MultiResolutionRegistrationWithFeatures/elxMultiResolutionRegistrationWithFeatures.hxx
+++ src/Components/Registrations/MultiResolutionRegistrationWithFeatures/elxMultiResolutionRegistrationWithFeatures.hxx
@@ -187,10 +187,8 @@ using namespace itk;
 		void MultiResolutionRegistrationWithFeatures<TElastix>
     ::UpdateFixedMasks( unsigned int level )
 	{    
-    /** some shortcuts. */
-    const unsigned int nrOfFixedMasks = this->GetElastix()->GetNumberOfFixedMasks();
-    const unsigned int nrOfFixedImages = this->GetElastix()->GetNumberOfFixedImages();
-    const unsigned int nrOfFixedImagePyramids = this->GetElastix()->GetNumberOfFixedImagePyramids();
+    /** Use only one mask. */
+    const unsigned int nrOfFixedImageMasks = 1;//this->GetElastix()->GetNumberOfFixedMasks();
 
     /** Array of bools, that remembers for each mask if erosion is wanted. */
     UseMaskErosionArrayType useMaskErosionArray;
@@ -202,65 +200,17 @@ using namespace itk;
     
     /** Read whether mask erosion is wanted, if any masks were supplied. */
     useMaskErosion = this->ReadMaskParameters( useMaskErosionArray,
-      nrOfFixedMasks, "Fixed", level );
+      nrOfFixedImageMasks, "Fixed", level );
 
     /** Create and start timer, to time the whole mask configuration procedure. */
     TimerPointer timer = TimerType::New();
     timer->StartTimer();
- 
-    /** Now set the masks. *
-    if (  ( nrOfFixedImages == 1 || nrOfFixedMasks == 0 ) &&
-      nrOfFixedMasks <= 1 &&
-      ( nrOfFixedImagePyramids == 1  || !useMaskErosion || nrOfFixedMasks == 0 ) )
-    {
-      /** 1 image || nomask, <= 1 mask, 1 pyramid || noerosion || nomask: 
-       * --> we can use one mask for all metrics! (or no mask at all).
-       *
-      FixedMaskSpatialObjectPointer fixedMask = this->GenerateFixedMaskSpatialObject( 
-        this->GetElastix()->GetFixedMask(), useMaskErosion,
-        this->GetFixedImagePyramid(), level );
-      this->GetCombinationMetric()->SetFixedImageMask( fixedMask );
-    }
-    else if ( nrOfFixedImages == 1 && nrOfFixedMasks == 1 )
-    {
-      /** 1 image, 1 mask, erosion && multiple pyramids
-       * Set a differently eroded mask in each metric. The eroded
-       * masks are all based on the same mask image, but generated with
-       * different pyramid settings.
-       *
-      for ( unsigned int i = 0; i < nrOfMetrics; ++i )
-      { 
-        FixedMaskSpatialObjectPointer fixedMask = this->GenerateFixedMaskSpatialObject( 
-          this->GetElastix()->GetFixedMask(), useMaskErosion,
-          this->GetFixedImagePyramid( i ), level );
-        this->GetCombinationMetric()->SetFixedImageMask( fixedMask, i );
-      }
-    }
-    else
-    {
-      /** All other cases. Note that the number of pyramids should equal 1 or
-       * should equal the number of metrics. 
-       * Set each supplied mask in its corresponding metric, possibly after erosion.
-       * If more metrics than masks are present, the last metrics will not use a mask.
-       * If less metrics than masks are present, the last masks will be ignored.
-       *
-      for ( unsigned int i = 0; i < nrOfMetrics; ++i)
-      {
-        bool useMask_i = false; // default value in case of more metrics than masks
-        if ( i < nrOfFixedMasks )
-        {
-          useMask_i = useMaskErosionArray[ i ];
-        }
-        FixedImagePyramidPointer pyramid_i = this->GetFixedImagePyramid(); // default value in case of only 1 pyramid
-        if ( i < nrOfFixedImagePyramids )
-        {
-          pyramid_i = this->GetFixedImagePyramid( i );
-        }
-        FixedMaskSpatialObjectPointer fixedMask = this->GenerateFixedMaskSpatialObject( 
-          this->GetElastix()->GetFixedMask( i ), useMask_i, pyramid_i, level );
-        this->GetCombinationMetric()->SetFixedImageMask( fixedMask, i );
-      }
-    } // end else
+
+    /** Set the fixed image mask. Only one mask is assumed here. */
+    FixedMaskSpatialObjectPointer fixedMask = this->GenerateFixedMaskSpatialObject(
+      this->GetElastix()->GetFixedMask(), useMaskErosion,
+      this->GetFixedImagePyramid(), level );
+    this->GetMultiInputMetric()->SetFixedImageMask( fixedMask );
 
     /** Stop timer and print the elapsed time. */
 		timer->StopTimer();
@@ -278,78 +228,31 @@ using namespace itk;
   template <class TElastix>
 		void MultiResolutionRegistrationWithFeatures<TElastix>
     ::UpdateMovingMasks( unsigned int level )
-	{    
-    /** some shortcuts */
-    const unsigned int nrOfMetrics = this->GetElastix()->GetNumberOfMetrics();
-    const unsigned int nrOfMovingMasks = this->GetElastix()->GetNumberOfMovingMasks();
-    const unsigned int nrOfMovingImages = this->GetElastix()->GetNumberOfMovingImages();
-    const unsigned int nrOfMovingImagePyramids = this->GetElastix()->GetNumberOfMovingImagePyramids();
+	{
+    /** Use only one mask. */
+    const unsigned int nrOfMovingImageMasks = 1;//this->GetElastix()->GetNumberOfMovingMasks();
 
-    /** Array of bools, that remembers for each mask if erosion is wanted */
+    /** Array of bools, that remembers for each mask if erosion is wanted. */
     UseMaskErosionArrayType useMaskErosionArray;
 
     /** Bool that remembers if mask erosion is wanted in any of the masks 
-     * remains false when no masks are used */
+     * remains false when no masks are used.
+     */
     bool useMaskErosion;
     
-    /** Read whether mask erosion is wanted, if any masks were supplied */
+    /** Read whether mask erosion is wanted, if any masks were supplied. */
     useMaskErosion = this->ReadMaskParameters( useMaskErosionArray,
-      nrOfMovingMasks, "Moving", level);
+      nrOfMovingImageMasks, "Moving", level );
     
     /** Create and start timer, to time the whole mask configuration procedure. */
     TimerPointer timer = TimerType::New();
     timer->StartTimer();
-        
-    /** Now set the masks *
-    if (  ( (nrOfMovingImages==1) || (nrOfMovingMasks==0) ) &&
-          (nrOfMovingMasks<=1) &&
-          ( (nrOfMovingImagePyramids==1) || !useMaskErosion || (nrOfMovingMasks==0) )   )
-    {      
-      /** 1 image || nomask, <=1 mask, 1 pyramid || noerosion || nomask: 
-       * --> we can use one mask for all metrics! (or no mask at all) *
-      MovingMaskSpatialObjectPointer movingMask = this->GenerateMovingMaskSpatialObject( 
-        this->GetElastix()->GetMovingMask(), useMaskErosion,
-        this->GetMovingImagePyramid(), level );
-      this->GetCombinationMetric()->SetMovingImageMask( movingMask );
-    } 
-    else if ( (nrOfMovingImages==1) && (nrOfMovingMasks==1) )
-    {
-      /** 1 image, 1 mask, erosion && multiple pyramids
-       * Set a differently eroded mask in each metric. The eroded
-       * masks are all based on the same mask image, but generated with
-       * different pyramid settings. *
-      for (unsigned int i = 0; i < nrOfMetrics; ++i)
-      { 
-        MovingMaskSpatialObjectPointer movingMask = this->GenerateMovingMaskSpatialObject( 
-          this->GetElastix()->GetMovingMask(), useMaskErosion,
-          this->GetMovingImagePyramid(i), level );
-        this->GetCombinationMetric()->SetMovingImageMask( movingMask, i );
-      }
-    }
-    else
-    {
-      /** All other cases. Note that the number of pyramids should equal 1 or
-       * should equal the number of metrics. 
-       * Set each supplied mask in its corresponding metric, possibly after erosion.
-       * If more metrics than masks are present, the last metrics will not use a mask.
-       * If less metrics than masks are present, the last masks will be ignored. *
-      for (unsigned int i = 0; i < nrOfMetrics; ++i)
-      {
-        bool useMask_i = false; // default value in case of more metrics than masks
-        if ( i < nrOfMovingMasks )
-        {
-          useMask_i = useMaskErosionArray[i];
-        }
-        MovingImagePyramidPointer pyramid_i = this->GetMovingImagePyramid(); // default value in case of only 1 pyramid
-        if (i < nrOfMovingImagePyramids)
-        {
-          pyramid_i = this->GetMovingImagePyramid(i);
-        }
-        MovingMaskSpatialObjectPointer movingMask = this->GenerateMovingMaskSpatialObject( 
-          this->GetElastix()->GetMovingMask(i), useMask_i, pyramid_i, level );
-        this->GetCombinationMetric()->SetMovingImageMask( movingMask, i );
-      }
-    } // end else
+
+    /** Set the moving image mask. Only one mask is assumed here. */
+    MovingMaskSpatialObjectPointer movingMask = this->GenerateMovingMaskSpatialObject(
+      this->GetElastix()->GetMovingMask(), useMaskErosion,
+      this->GetMovingImagePyramid(), level );
+    this->GetMultiInputMetric()->SetMovingImageMask( movingMask );
 
     /** Stop timer and print the elapsed time. */
 		timer->StopTimer();
