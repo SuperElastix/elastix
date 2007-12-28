1f850a2ad0f4d1026793de18165e71b97ca6243b
Marius Staring <m.staring@lumc.nl>
2007-12-28 11:48:14 +0000

    MS:
    - ENH: changed elastix version to 3.803 with the commit and changes of the multiple input sampler. These changes should not affect classes that do not use the multiple input metric or sampler. So if I did something wrong it will only hurt me :-)

--- src/Core/Install/elxBaseComponent.h
+++ src/Core/Install/elxBaseComponent.h
@@ -23,7 +23,7 @@
 #include <sstream>
 
 /** The current Elastix version. */
-#define __ELASTIX_VERSION 3.802
+#define __ELASTIX_VERSION 3.803
 
 /** All Elastix components should be in namespace elastix. */
 namespace elastix
