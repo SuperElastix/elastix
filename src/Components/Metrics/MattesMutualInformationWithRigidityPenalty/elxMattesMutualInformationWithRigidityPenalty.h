da242f3f2a3428e35406e2d143b806323efa5d44
Marius Staring <m.staring@lumc.nl>
2008-01-02 15:34:28 +0000

    MS:
    - BUG: typenames needed for gcc

--- src/Components/Metrics/MattesMutualInformationWithRigidityPenalty/elxMattesMutualInformationWithRigidityPenalty.h
+++ src/Components/Metrics/MattesMutualInformationWithRigidityPenalty/elxMattesMutualInformationWithRigidityPenalty.h
@@ -178,26 +178,55 @@ using namespace itk;
 		elxClassNameMacro( "MattesMutualInformationWithRigidityPenalty" );
 
 		/** Typedefs inherited from the superclass. */
-		typedef typename Superclass1::TransformType							TransformType;
-		typedef typename Superclass1::TransformPointer 					TransformPointer;
-		typedef typename Superclass1::TransformJacobianType			TransformJacobianType;
-		typedef typename Superclass1::InterpolatorType 					InterpolatorType;
-		typedef typename Superclass1::MeasureType								MeasureType;
-		typedef typename Superclass1::DerivativeType 						DerivativeType;
-		typedef typename Superclass1::ParametersType 						ParametersType;
-		typedef typename Superclass1::FixedImageType 						FixedImageType;
-		typedef typename Superclass1::MovingImageType						MovingImageType;
-		typedef typename Superclass1::FixedImageConstPointer 		FixedImageConstPointer;
-		typedef typename Superclass1::MovingImageConstPointer		MovingImageConstPointer;
-		typedef typename Superclass1::FixedImageIndexType				FixedImageIndexType;
-		typedef typename Superclass1::FixedImageIndexValueType 	FixedImageIndexValueType;
-		typedef typename Superclass1::MovingImageIndexType 			MovingImageIndexType;
-		typedef typename Superclass1::FixedImagePointType				FixedImagePointType;
-		typedef typename Superclass1::MovingImagePointType 			MovingImagePointType;
+    typedef typename 
+      Superclass1::CoordinateRepresentationType              CoordinateRepresentationType;
+    typedef typename Superclass1::MovingImageType            MovingImageType;
+    typedef typename Superclass1::MovingImagePixelType       MovingImagePixelType;
+    typedef typename Superclass1::MovingImageConstPointer    MovingImageConstPointer;
+    typedef typename Superclass1::FixedImageType             FixedImageType;
+    typedef typename Superclass1::FixedImageConstPointer     FixedImageConstPointer;
+    typedef typename Superclass1::FixedImageRegionType       FixedImageRegionType;
+    typedef typename Superclass1::TransformType              TransformType;
+    typedef typename Superclass1::TransformPointer           TransformPointer;
+    typedef typename Superclass1::InputPointType             InputPointType;
+    typedef typename Superclass1::OutputPointType            OutputPointType;
+    typedef typename Superclass1::TransformParametersType    TransformParametersType;
+    typedef typename Superclass1::TransformJacobianType      TransformJacobianType;
+    typedef typename Superclass1::InterpolatorType           InterpolatorType;
+    typedef typename Superclass1::InterpolatorPointer        InterpolatorPointer;
+    typedef typename Superclass1::RealType                   RealType;
+    typedef typename Superclass1::GradientPixelType          GradientPixelType;
+    typedef typename Superclass1::GradientImageType          GradientImageType;
+    typedef typename Superclass1::GradientImagePointer       GradientImagePointer;
+    typedef typename Superclass1::GradientImageFilterType    GradientImageFilterType;
+    typedef typename Superclass1::GradientImageFilterPointer GradientImageFilterPointer;
+    typedef typename Superclass1::FixedImageMaskType         FixedImageMaskType;
+    typedef typename Superclass1::FixedImageMaskPointer      FixedImageMaskPointer;
+    typedef typename Superclass1::MovingImageMaskType        MovingImageMaskType;
+    typedef typename Superclass1::MovingImageMaskPointer     MovingImageMaskPointer;
+    typedef typename Superclass1::MeasureType                MeasureType;
+    typedef typename Superclass1::DerivativeType             DerivativeType;
+    typedef typename Superclass1::ParametersType             ParametersType;
+    typedef typename Superclass1::FixedImagePixelType        FixedImagePixelType;
+    typedef typename Superclass1::MovingImageRegionType      MovingImageRegionType;
+    typedef typename Superclass1::ImageSamplerType           ImageSamplerType;
+    typedef typename Superclass1::ImageSamplerPointer        ImageSamplerPointer;
+    typedef typename Superclass1::ImageSampleContainerType   ImageSampleContainerType;
+    typedef typename 
+      Superclass1::ImageSampleContainerPointer               ImageSampleContainerPointer;
+    typedef typename Superclass1::FixedImageLimiterType      FixedImageLimiterType;
+    typedef typename Superclass1::MovingImageLimiterType     MovingImageLimiterType;
+    typedef typename
+      Superclass1::FixedImageLimiterOutputType               FixedImageLimiterOutputType;
+    typedef typename
+      Superclass1::MovingImageLimiterOutputType              MovingImageLimiterOutputType;
+    typedef typename
+      Superclass1::MovingImageDerivativeScalesType           MovingImageDerivativeScalesType;
 		
 		/** The fixed image dimension. */
 		itkStaticConstMacro (FixedImageDimension, unsigned int,
 			FixedImageType::ImageDimension);
+
 		/** The moving image dimension. */
 		itkStaticConstMacro( MovingImageDimension, unsigned int,
 			MovingImageType::ImageDimension );
