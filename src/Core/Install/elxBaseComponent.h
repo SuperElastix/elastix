8f2751b7be0f3691d374fd825b13b8d976d57c60
Marius Staring <m.staring@lumc.nl>
2008-05-28 09:00:52 +0000

    MS:
    - ENH: increased elastix version to 3.901 after the bounding box support change.

--- src/Core/Install/elxBaseComponent.h
+++ src/Core/Install/elxBaseComponent.h
@@ -37,7 +37,7 @@
 #include <sstream>
 
 /** The current Elastix version. */
-#define __ELASTIX_VERSION 3.900
+#define __ELASTIX_VERSION 3.901
 
 /** All Elastix components should be in namespace elastix. */
 namespace elastix
