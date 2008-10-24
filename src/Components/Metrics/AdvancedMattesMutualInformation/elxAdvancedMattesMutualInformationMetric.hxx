fa25f78f5c71682503820193cf0cd0b9b12a960d
Marius Staring <m.staring@lumc.nl>
2008-10-24 15:57:24 +0000

    MS:
    - STYLE: several

--- src/Components/Metrics/AdvancedMattesMutualInformation/elxAdvancedMattesMutualInformationMetric.hxx
+++ src/Components/Metrics/AdvancedMattesMutualInformation/elxAdvancedMattesMutualInformationMetric.hxx
@@ -130,8 +130,10 @@ using namespace itk;
     {
       double c = 1.0;
       double gamma = 0.101;
-      this->GetConfiguration()->ReadParameter(c, "SP_c", this->GetComponentLabel(), level, 0 );
-      this->GetConfiguration()->ReadParameter(gamma, "SP_gamma", this->GetComponentLabel(), level, 0 );
+      this->GetConfiguration()->ReadParameter( c, "SP_c",
+        this->GetComponentLabel(), level, 0 );
+      this->GetConfiguration()->ReadParameter( gamma, "SP_gamma",
+        this->GetComponentLabel(), level, 0 );
       this->SetParam_c( c );
       this->SetParam_gamma( gamma );
       this->SetFiniteDifferencePerturbation( this->Compute_c( 0 ) );
@@ -142,14 +144,16 @@ using namespace itk;
     int usescales = 0;
     for ( unsigned int i = 0; i < MovingImageDimension; ++i )
     {
-      usescales |= this->GetConfiguration()->ReadParameter( movingImageDerivativeScales[i],
-        "MovingImageDerivativeScales", this->GetComponentLabel(), i, -1, true );
+      usescales |= this->GetConfiguration()->ReadParameter(
+        movingImageDerivativeScales[ i ], "MovingImageDerivativeScales",
+        this->GetComponentLabel(), i, -1, true );
     }
     if ( usescales == 0 )
     {
       this->SetUseMovingImageDerivativeScales( true );
       this->SetMovingImageDerivativeScales( movingImageDerivativeScales );
-      elxout << "Multiplying moving image derivatives by: " << movingImageDerivativeScales << std::endl;
+      elxout << "Multiplying moving image derivatives by: "
+        << movingImageDerivativeScales << std::endl;
     }
     
   } // end BeforeEachResolution()
