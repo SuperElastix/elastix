c1812b07ed630b0cd6e69dd49decb8d0f4bbb762
Marius Staring <m.staring@lumc.nl>
2008-11-19 15:45:09 +0000

    MS:
    - ENH: added a check for zero order B-spline interpolation

--- src/Components/Interpolators/BSplineInterpolatorFloat/elxBSplineInterpolatorFloat.hxx
+++ src/Components/Interpolators/BSplineInterpolatorFloat/elxBSplineInterpolatorFloat.hxx
@@ -22,29 +22,36 @@ namespace elastix
 using namespace itk;
 
 
-  /**
-   * ***************** BeforeEachResolution ***********************
-   */
+/**
+ * ***************** BeforeEachResolution ***********************
+ */
 
-  template <class TElastix>
-    void BSplineInterpolatorFloat<TElastix>::
-    BeforeEachResolution( void )
+template <class TElastix>
+void BSplineInterpolatorFloat<TElastix>
+::BeforeEachResolution( void )
+{
+  /** Get the current resolution level. */
+  unsigned int level
+    = ( this->m_Registration->GetAsITKBaseType() )->GetCurrentLevel();
+
+  /** Read the desired spline order from the parameter file. */
+  unsigned int splineOrder = 1;
+  this->GetConfiguration()->ReadParameter( splineOrder,
+    "BSplineInterpolationOrder", this->GetComponentLabel(), level, 0 );
+
+  /** Check. */
+  if ( splineOrder == 0 )
   {
-    /** Get the current resolution level. */
-    unsigned int level = 
-      ( this->m_Registration->GetAsITKBaseType() )->GetCurrentLevel();
+    elx::xout["warning"] << "WARNING: the BSplineInterpolationOrder is set to 0.\n"
+      << "         It is not possible to take derivatives with this setting.\n"
+      << "         Make sure you use a derivative free optimizer."
+      << std::endl;
+  }
 
-    /** Set the SplineOrder, default value = 1. */
-    unsigned int splineOrder = 1;
-    
-    /** Read the desired splineOrder from the parameterFile. */
-    this->GetConfiguration()->ReadParameter( splineOrder,
-      "BSplineInterpolationOrder", this->GetComponentLabel(), level, 0 );
-    
-    /** Set the splineOrder. */
-    this->SetSplineOrder( splineOrder );
-     
-  } // end BeforeEachResolution
+  /** Set the splineOrder. */
+  this->SetSplineOrder( splineOrder );
+
+} // end BeforeEachResolution()
 
 
 } // end namespace elastix
