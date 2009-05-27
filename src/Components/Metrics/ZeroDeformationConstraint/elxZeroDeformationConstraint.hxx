c8589c9684ab34813eb7ecf6581ea6d9b711e0b1
Coert Metz <coert.metz@gmail.com>
2009-05-27 09:19:58 +0000

    CM:
    - ADD: ZeroDeformationConstraint which penalizes deformation at the gridpoints using an augmented Lagrangian penalty term.

---
+++ src/Components/Metrics/ZeroDeformationConstraint/elxZeroDeformationConstraint.hxx
@@ -0,0 +1,179 @@
+/*======================================================================
+
+  This file is part of the elastix software.
+
+  Copyright (c) University Medical Center Utrecht. All rights reserved.
+  See src/CopyrightElastix.txt or http://elastix.isi.uu.nl/legal.php for
+  details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE. See the above copyright notices for more information.
+
+======================================================================*/
+
+#ifndef __elxZeroDeformationConstraint_HXX__
+#define __elxZeroDeformationConstraint_HXX__
+
+#include "elxZeroDeformationConstraint.h"
+
+
+namespace elastix
+{
+using namespace itk;
+
+  /**
+   * ******************* Initialize ***********************
+   */
+
+  template <class TElastix>
+    void ZeroDeformationConstraint<TElastix>
+    ::Initialize(void) throw (ExceptionObject)
+  {
+
+    TimerPointer timer = TimerType::New();
+    timer->StartTimer();
+    this->Superclass1::Initialize();
+    timer->StopTimer();
+    elxout << "Initialization of ZeroDeformationConstraintMetric metric took: "
+      << static_cast<long>( timer->GetElapsedClockSec() * 1000 ) << " ms." << std::endl;
+
+  } // end Initialize
+
+
+  /**
+  * ***************** BeforeRegistration ***********************
+  */
+
+  template <class TElastix>
+  void ZeroDeformationConstraint<TElastix>
+    ::BeforeRegistration(void)
+  {
+    
+    /** Add columns for the penalty term and lagrange multipliers. */
+    xout["iteration"].AddTargetCell("5:Penalty");
+    xout["iteration"].AddTargetCell("6:Lagrange");
+    xout["iteration"].AddTargetCell("7:Infeasibility");
+
+    /** Format the metric and stepsize as floats */     
+    xl::xout["iteration"]["5:Penalty"]  << std::showpoint << std::fixed;
+    xl::xout["iteration"]["6:Lagrange"] << std::showpoint << std::fixed;
+    xl::xout["iteration"]["7:Infeasibility"] << std::showpoint << std::fixed;
+
+  }
+
+  /**
+   * ***************** BeforeEachResolution ***********************
+   */
+
+  template <class TElastix>
+    void ZeroDeformationConstraint<TElastix>
+    ::BeforeEachResolution(void)
+  {
+    /** Reset iteration number. */
+    this->m_CurrentIteration = 0;
+
+    /** Get the current resolution level. */
+    unsigned int level = 
+      ( this->m_Registration->GetAsITKBaseType() )->GetCurrentLevel();
+
+    /** Get the initial lagrange multiplier. */
+    float initialLagrangeMultiplier;
+    this->GetConfiguration()->ReadParameter( initialLagrangeMultiplier,
+      "InitialLagrangeMultiplier", this->GetComponentLabel(), level, 0 );
+    this->SetInitialLangrangeMultiplier( initialLagrangeMultiplier );
+    this->m_AverageLagrangeMultiplier = this->m_InitialLangrangeMultiplier;
+
+    /** Get the initial penalty term multiplier. */
+    this->GetConfiguration()->ReadParameter( this->m_InitialPenaltyTermMultiplier,
+      "InitialPenaltyTermMultiplier", this->GetComponentLabel(), level, 0 );
+    this->m_CurrentPenaltyTermMultiplier = this->m_InitialPenaltyTermMultiplier;
+    
+    /** Get the penalty term multiplier factor. */
+    this->GetConfiguration()->ReadParameter( this->m_PenaltyTermMultiplierFactor,
+      "PenaltyTermMultiplierFactor", this->GetComponentLabel(), level, 0 );
+
+    /** Get the number of subiterations after which the new lagrange multiplier
+     * is determined.
+     */
+    this->GetConfiguration()->ReadParameter( this->m_NumSubIterations,
+      "NumberOfSubIterations", this->GetComponentLabel(), level, 0 );
+
+    /** Set moving image derivative scales. */
+    this->SetUseMovingImageDerivativeScales( false );
+    MovingImageDerivativeScalesType movingImageDerivativeScales;
+    bool usescales = true;
+    for ( unsigned int i = 0; i < MovingImageDimension; ++i )
+    {
+      usescales = usescales && this->GetConfiguration()->ReadParameter(
+        movingImageDerivativeScales[ i ], "MovingImageDerivativeScales",
+        this->GetComponentLabel(), i, -1, true );
+    }
+    if ( usescales )
+    {
+      this->SetUseMovingImageDerivativeScales( true );
+      this->SetMovingImageDerivativeScales( movingImageDerivativeScales );
+      elxout << "Multiplying moving image derivatives by: "
+        << movingImageDerivativeScales << std::endl;
+    }
+    
+  } // end BeforeEachResolution
+
+  template <class TElastix>
+  void ZeroDeformationConstraint<TElastix>
+    ::AfterEachIteration(void)
+  {
+    this->m_CurrentIteration++;
+
+    /** Print some information */
+    xl::xout["iteration"]["5:Penalty"]  << this->m_CurrentPenaltyTermMultiplier;
+    xl::xout["iteration"]["6:Lagrange"] << this->m_AverageLagrangeMultiplier;
+    xl::xout["iteration"]["7:Infeasibility"] << this->GetCurrentInfeasibility();
+
+    this->m_CurrentPenaltyTermMultiplier = this->DetermineNewPenaltyTermMultiplier( m_CurrentIteration );
+    if ( m_CurrentIteration % this->m_NumSubIterations == 0 )
+    {
+      this->DetermineNewLagrangeMultipliers();
+    }    
+  } // end AfterEachIteration
+
+
+  /**
+  * ******************* DetermineNewLagrangeMultipliers *******************
+  */
+
+  template <class TElastix>
+  void ZeroDeformationConstraint<TElastix>
+    ::DetermineNewLagrangeMultipliers( )
+  {
+
+    m_AverageLagrangeMultiplier = 0.0;
+    for ( std::vector< float >::size_type i = 0; i < this->m_CurrentLagrangeMultipliers.size(); ++i )
+    {
+      this->m_CurrentLagrangeMultipliers[ i ] = min( 0.0f, this->m_CurrentLagrangeMultipliers[ i ] - 
+        this->m_CurrentPenaltyTermValues[ i ] * this->GetCurrentPenaltyTermMultiplier() );
+      m_AverageLagrangeMultiplier += this->m_CurrentLagrangeMultipliers[ i ];
+    }
+    m_AverageLagrangeMultiplier /= static_cast< double > ( this->m_CurrentLagrangeMultipliers.size() );
+
+  } // end DetermineNewLagrangeMultipliers
+
+
+  /**
+  * ******************* DetermineNewPenaltyTermMultiplier *******************
+  */
+
+  template <class TElastix>
+  float ZeroDeformationConstraint<TElastix>
+    ::DetermineNewPenaltyTermMultiplier( const int iterationNumber ) const 
+  {
+    return static_cast<float> ( this->m_InitialPenaltyTermMultiplier * vcl_pow( 
+      static_cast<double>( this->m_PenaltyTermMultiplierFactor ), static_cast<double> ( iterationNumber ) ) );
+  } // end DetermineNewTermPenaltyMultiplier
+
+
+} // end namespace elastix
+
+
+#endif // end #ifndef __elxZeroDeformationConstraint_HXX__
+
---
+++ src/Components/Metrics/ZeroDeformationConstraint/itkZeroDeformationConstraint.hxx
@@ -0,0 +1,312 @@
+/*======================================================================
+
+  This file is part of the elastix software.
+
+  Copyright (c) University Medical Center Utrecht. All rights reserved.
+  See src/CopyrightElastix.txt or http://elastix.isi.uu.nl/legal.php for
+  details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE. See the above copyright notices for more information.
+
+======================================================================*/
+
+#ifndef __itkZeroDeformationConstraintMetric_hxx
+#define __itkZeroDeformationConstraintMetric_hxx
+
+#include "itkZeroDeformationConstraint.h"
+
+namespace itk
+{
+
+  /**
+  * ******************* Constructor *******************
+  */
+
+  template <class TFixedImage, class TMovingImage> 
+    ZeroDeformationConstraintMetric<TFixedImage,TMovingImage>
+      ::ZeroDeformationConstraintMetric():
+        m_CurrentPenaltyTermMultiplier( 1.0f ), 
+        m_CurrentLagrangeMultipliers( 0 ),
+        m_InitialLangrangeMultiplier( 1.0f )
+  {
+    this->SetUseImageSampler( true );
+    this->SetUseFixedImageLimiter( false );
+    this->SetUseMovingImageLimiter( false );
+
+  } // end constructor
+
+
+  /**
+   * ******************* Initialize *******************
+   */
+
+  template <class TFixedImage, class TMovingImage>
+    void
+    ZeroDeformationConstraintMetric<TFixedImage,TMovingImage>
+    ::Initialize(void) throw ( ExceptionObject )
+  {
+    
+    /** Initialize transform, interpolator, etc. */
+    Superclass::Initialize();
+
+    /** Set right size of lagrangian multiplier vector and initialize to
+     * initial langrange multiplier value as supplied by the user. 
+     */
+    this->GetImageSampler()->Update();
+    this->m_CurrentLagrangeMultipliers.clear();
+    this->m_CurrentLagrangeMultipliers.resize( this->GetImageSampler()->GetOutput()->Size(), this->m_InitialLangrangeMultiplier );
+
+    /** Set right size of current penalty term value vector. These values are
+     * used to determine new langrange multipliers.
+     */
+    this->m_CurrentPenaltyTermValues.clear();
+    this->m_CurrentPenaltyTermValues.resize( this->GetImageSampler()->GetOutput()->Size(), 0.0f );
+
+  } // end Initialize
+
+
+  /**
+   * ******************* PrintSelf *******************
+   */
+  
+  template < class TFixedImage, class TMovingImage> 
+    void
+    ZeroDeformationConstraintMetric<TFixedImage,TMovingImage>
+    ::PrintSelf(std::ostream& os, Indent indent) const
+  {
+    Superclass::PrintSelf( os, indent );
+
+  } // end PrintSelf
+
+
+  /**
+   * ******************* GetValue *******************
+   */
+
+  template <class TFixedImage, class TMovingImage> 
+    typename ZeroDeformationConstraintMetric<TFixedImage,TMovingImage>::MeasureType
+    ZeroDeformationConstraintMetric<TFixedImage,TMovingImage>
+    ::GetValue( const TransformParametersType & parameters ) const
+  {
+    itkDebugMacro( "GetValue( " << parameters << " ) " );
+
+    /** Make sure the transform parameters are up to date. */
+    this->SetTransformParameters( parameters );
+
+    /** Update the imageSampler and get a handle to the sample container. */
+    this->GetImageSampler()->Update();
+    ImageSampleContainerPointer sampleContainer = this->GetImageSampler()->GetOutput();
+
+    /** Create iterator over the sample container. */
+    typename ImageSampleContainerType::ConstIterator fiter;
+    typename ImageSampleContainerType::ConstIterator fbegin = sampleContainer->Begin();
+    typename ImageSampleContainerType::ConstIterator fend = sampleContainer->End();
+
+    /** Initialize some variables. */
+    double sumMagnitude = 0.0;
+    double sumSquaredMagnitude = 0.0;
+    this->m_CurrentInfeasibility = 0.0;
+    this->m_NumberOfPixelsCounted = 0;
+
+    /** Loop over the samples to compute sums for computation of the metric. */
+    int i = 0;
+    for ( fiter = fbegin; fiter != fend; ++fiter )
+    {
+      /** Read fixed coordinates. */
+      FixedImagePointType fixedPoint = (*fiter).Value().m_ImageCoordinates;
+
+      /** Transform point and check if it is inside the b-spline support region. */
+      MovingImagePointType mappedPoint;
+      bool sampleOk = this->TransformPoint( fixedPoint, mappedPoint );
+
+      /** Check if point is inside mask. */
+      if ( sampleOk )
+      {
+        sampleOk = this->IsInsideMovingMask( mappedPoint );
+      }
+
+      /** Compute the magnitude of mappedPoint - fixedPoint. */
+      if (sampleOk)
+      {
+        this->m_NumberOfPixelsCounted++;
+
+        const float magnitude = (mappedPoint - fixedPoint).GetVnlVector().magnitude();
+        /** Remember current penalty term value. */
+        this->m_CurrentPenaltyTermValues[i] = magnitude;
+        /** Update magnitude sums. */
+        sumMagnitude += this->m_CurrentLagrangeMultipliers[ i ] * magnitude;
+        sumSquaredMagnitude += magnitude*magnitude;
+        /** Update infeasibility value. */
+        this->m_CurrentInfeasibility += magnitude;
+      }
+      ++i;
+    } // end for loop over the image sample container
+
+    /** Average infeasibility measure. */
+    this->m_CurrentInfeasibility /= this->m_NumberOfPixelsCounted;
+
+    /** Check if enough samples were valid. */
+    this->CheckNumberOfSamples(
+      sampleContainer->Size(), this->m_NumberOfPixelsCounted );
+
+    /** Return the mean squares measure value. */
+    return ( -sumMagnitude + this->m_CurrentPenaltyTermMultiplier / 2.0 * sumSquaredMagnitude ) / static_cast< float > ( m_NumberOfPixelsCounted );
+    //return -sumMagnitude + this->m_CurrentPenaltyTermMultiplier / 2.0 * sumSquaredMagnitude;
+
+  } // end GetValue
+  
+
+  /**
+   * ******************* GetDerivative *******************
+   */
+
+  template < class TFixedImage, class TMovingImage> 
+    void
+    ZeroDeformationConstraintMetric<TFixedImage,TMovingImage>
+    ::GetDerivative( const TransformParametersType & parameters,
+    DerivativeType & derivative ) const
+  {
+    /** When the derivative is calculated, all information for calculating
+     * the metric value is available. It does not cost anything to calculate
+     * the metric value now. Therefore, we have chosen to only implement the
+     * GetValueAndDerivative(), supplying it with a dummy value variable. */
+    MeasureType dummyvalue = NumericTraits< MeasureType >::Zero;
+    this->GetValueAndDerivative( parameters, dummyvalue, derivative );
+
+  } // end GetDerivative
+
+
+  /**
+   * ******************* GetValueAndDerivative *******************
+   */
+
+  template <class TFixedImage, class TMovingImage>
+    void
+    ZeroDeformationConstraintMetric<TFixedImage,TMovingImage>
+    ::GetValueAndDerivative( const TransformParametersType & parameters, 
+    MeasureType & value, DerivativeType & derivative ) const
+  {
+    itkDebugMacro( "GetValueAndDerivative( " << parameters << " ) ");
+
+    const float epsilon = 1.0E-14f;
+
+    /** Define derivative and Jacobian types. */
+    typedef typename DerivativeType::ValueType        DerivativeValueType;
+    typedef typename TransformJacobianType::ValueType TransformJacobianValueType;
+
+    /** Initialize measure and derivative to zero. */
+    MeasureType measure = NumericTraits< MeasureType >::Zero;
+    derivative = DerivativeType( this->GetNumberOfParameters() );
+    derivative.Fill( NumericTraits< DerivativeValueType >::Zero );
+
+    /** Arrays that store dM(x)/dmu. */
+    DerivativeType imageJacobian( this->m_NonZeroJacobianIndices.size() );
+
+    /** Make sure the transform parameters are up to date. */
+    this->SetTransformParameters( parameters );
+
+    /** Update the imageSampler and get a handle to the sample container. */
+    this->GetImageSampler()->Update();
+    ImageSampleContainerPointer sampleContainer = this->GetImageSampler()->GetOutput();
+
+    /** Create iterator over the sample container. */
+    typename ImageSampleContainerType::ConstIterator fiter;
+    typename ImageSampleContainerType::ConstIterator fbegin = sampleContainer->Begin();
+    typename ImageSampleContainerType::ConstIterator fend = sampleContainer->End();
+
+    /** Initialize some variables. */
+    double sumMagnitude = 0.0;
+    double sumSquaredMagnitude = 0.0;
+    double sumDerivative = 0.0;
+    this->m_CurrentInfeasibility = 0.0;
+    this->m_NumberOfPixelsCounted = 0;
+
+    /** Loop over the samples to compute sums for the metric value and derivative. */
+    int i = 0;
+    for ( fiter = fbegin; fiter != fend; ++fiter )
+    {
+      /** Read fixed coordinates. */
+      FixedImagePointType fixedPoint = (*fiter).Value().m_ImageCoordinates;
+
+      /** Transform point and check if it is inside the bspline support region. */
+      MovingImagePointType mappedPoint;
+      bool sampleOk = this->TransformPoint( fixedPoint, mappedPoint );
+
+      /** Check if point is inside mask. */
+      if ( sampleOk )
+      {
+        sampleOk = this->IsInsideMovingMask( mappedPoint );
+      }
+
+      /** Compute the magnitude of mappedPoint - fixedPoint. */
+      if (sampleOk)
+      {
+        this->m_NumberOfPixelsCounted++;
+
+        /** Update terms for value. */
+        MovingImagePointType transformation;
+        for ( unsigned int d = 0; d < FixedImageDimension; ++d )
+        {
+          transformation[ d ] = mappedPoint[ d ] - fixedPoint[ d ];
+        }
+        const float magnitude = transformation.GetVnlVector().magnitude();
+        /** Remember current penalty term value. */
+        this->m_CurrentPenaltyTermValues[ i ] = magnitude;
+        /** Update magnitude sums. */
+        sumMagnitude += this->m_CurrentLagrangeMultipliers[ i ] * magnitude;
+        sumSquaredMagnitude += magnitude*magnitude;
+        /** Update infeasibility measure. */
+        this->m_CurrentInfeasibility += magnitude;
+
+        for ( unsigned int d = 0; d < FixedImageDimension; ++d )
+        {
+           transformation[ d ] = ( -this->m_CurrentLagrangeMultipliers[ i ] * transformation[ d ] )
+              / ( magnitude + epsilon ) + this->m_CurrentPenaltyTermMultiplier * transformation[ d ];
+        }
+
+        /** Get the TransformJacobian dT/dMu (jacobian). */
+        const TransformJacobianType & jacobian = 
+            this->EvaluateTransformJacobian( fixedPoint );
+
+        /** Compute (dT/dMu)T * scaledTransformation. */
+        const NonZeroJacobianIndicesType::size_type numNonZeroJacobianIndices = this->m_NonZeroJacobianIndices.size();
+        for ( NonZeroJacobianIndicesType::size_type n = 0; n < numNonZeroJacobianIndices; ++n )
+        {
+          float jacScaledTransformation = 0.0f;
+          for ( unsigned int d = 0; d < FixedImageDimension; ++d )
+          {
+            jacScaledTransformation += jacobian( d, n ) * transformation[ d ];
+          }
+
+          /** Update derivative sum. */
+          derivative[ this->m_NonZeroJacobianIndices[ n ] ] += jacScaledTransformation;
+        }
+      } // sampleOk
+      i++;
+    } // end for loop over the image sample container
+
+    /** Check if enough samples were valid. */
+    this->CheckNumberOfSamples(
+      sampleContainer->Size(), this->m_NumberOfPixelsCounted );
+
+    /** Average infeasibility measure. */
+    this->m_CurrentInfeasibility /= this->m_NumberOfPixelsCounted;
+
+    /** Return the mean squares measure value. */
+    //value = -sumMagnitude + this->m_CurrentPenaltyTermMultiplier / 2.0 * sumSquaredMagnitude;
+    value = ( -sumMagnitude + this->m_CurrentPenaltyTermMultiplier / 2.0 * sumSquaredMagnitude ) / static_cast< float > ( m_NumberOfPixelsCounted );
+
+    for ( int p = 0; p < this->GetNumberOfParameters(); ++p) 
+    {
+      derivative[ p ] /= static_cast< float > ( m_NumberOfPixelsCounted );
+    }
+
+  } // end GetValueAndDerivative()
+
+
+} // end namespace itk
+
+#endif // end #ifndef _itkZeroDeformationConstraintMetric_hxx
+
