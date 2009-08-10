3b549ceed493700f23a8a8fe5fbd072d7a32c606
Marius Staring <m.staring@lumc.nl>
2009-08-10 15:45:42 +0000

    MS:
    -BUG: the second template argument should be scalartype and not the moving image. We got away with it, because it was not used.

--- src/Components/Metrics/BendingEnergyPenalty/elxTransformBendingEnergyPenaltyTerm.h
+++ src/Components/Metrics/BendingEnergyPenalty/elxTransformBendingEnergyPenaltyTerm.h
@@ -40,7 +40,7 @@ using namespace itk;
     public
       TransformBendingEnergyPenaltyTerm<
         ITK_TYPENAME MetricBase<TElastix>::FixedImageType,
-        ITK_TYPENAME MetricBase<TElastix>::MovingImageType >,
+        double >,
     public MetricBase<TElastix>
   {
   public:
@@ -49,7 +49,7 @@ using namespace itk;
     typedef TransformBendingEnergyPenalty                 Self;
     typedef TransformBendingEnergyPenaltyTerm<
       typename MetricBase<TElastix>::FixedImageType,
-      typename MetricBase<TElastix>::MovingImageType >    Superclass1;
+      double >                                            Superclass1;
     typedef MetricBase<TElastix>                          Superclass2;
     typedef SmartPointer<Self>                            Pointer;
     typedef SmartPointer<const Self>                      ConstPointer;
