8868918b1f6ba79cee751c3f390e8de5a522e31d
Marius Staring <m.staring@lumc.nl>
2010-06-10 10:40:48 +0000

    MS:
    -ENH: increase elastix version to 4.301 to know what has been used for the EMPIRE10 challenge

--- src/Core/Install/elxBaseComponent.h
+++ src/Core/Install/elxBaseComponent.h
@@ -37,7 +37,7 @@
 #include <sstream>
 
 /** The current elastix version. */
-#define __ELASTIX_VERSION 4.300
+#define __ELASTIX_VERSION 4.301
 
 /** All elastix components should be in namespace elastix. */
 namespace elastix
