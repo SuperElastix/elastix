3dc114ea72875e3c41eb05adb295626d577a89f9
Stefan Klein <s.klein@erasmusmc.nl>
2011-01-03 16:34:26 +0000

    SK:
    -STY: svn properties were not set correctly
    -ADD: script for dashboard testing on linux64 cluster at BIGR

--- src/Testing/dashboard_script_SK_linux64.cmake
+++ src/Testing/dashboard_script_SK_linux64.cmake
@@ -1,21 +1,21 @@
 #
 # This script runs a dashboard
+# Usage:
+#   ctest -S <nameofthisscript>
 #
-# It has 1 optional argument: the build model.
-# The build model should be one of {Experimental, Continuous, Nightly}
-# and defaults to Nightly.
-#
-# Setup: Windows 7, Visual Studio 9 2008 Win64, Release mode, ITK 3.20.0
-# PC: LKEB, MS personal computer
+# Setup: linux 64bit,
+# gcc 4.1.2 20061115 (prerelease) (SUSE Linux),
+# Release mode, ITK 3.20.0
+# PC: linux cluster at BIGR (SK).
 
 
 # Where to find the src directory, and where to build the binaries
-SET( CTEST_SOURCE_DIRECTORY "D:/toolkits/elastix/nightly/elastix_src/src" )
-SET( CTEST_BINARY_DIRECTORY "D:/toolkits/elastix/nightly/elastix_bin" )
+SET( CTEST_SOURCE_DIRECTORY "/home/sklein/tk/elastix/nightlybuild/src" )
+SET( CTEST_CHECKOUT_DIRECTORY "/home/sklein/tk/elastix/nightlybuild" )
+SET( CTEST_BINARY_DIRECTORY "/home/sklein/tk/elastix/nightlybuild/release" )
 
 # Where to find CMake on my system
-SET( CTEST_CMAKE_COMMAND
-  "C:/Program Files (x86)/CMake 2.8/bin/cmake.exe" )
+SET( CTEST_CMAKE_COMMAND "/home/sklein/pk/install/bin/cmake" )
 
 # Specify the kind of dashboard to submit
 SET( MODEL Nightly )
@@ -25,59 +25,53 @@ ELSEIF( ${CTEST_SCRIPT_ARG} MATCHES Continuous )
   SET( MODEL Continuous )
 ENDIF()
 SET( CTEST_COMMAND
-  "C:/Program Files (x86)/CMake 2.8/bin/ctest.exe -D ${MODEL}" )
-# "C:/Program Files (x86)/CMake 2.8/bin/ctest.exe -D Experimental" )
-# "C:/Program Files (x86)/CMake 2.8/bin/ctest.exe -D Continuous" )
-# "C:/Program Files (x86)/CMake 2.8/bin/ctest.exe -D Nightly" )
+  "/home/sklein/pk/install/bin/ctest -D ${MODEL}" )
 
-# Specify svn update command
+# Specify svn update command;
+SET( CTEST_SVN_COMMAND "/usr/bin/svn" )
+SET( $ENV{LC_MESSAGES}    "en_EN" )
+set( CTEST_UPDATE_COMMAND "${CTEST_SVN_COMMAND}" )
 SET( CTEST_SVN_CHECKOUT
-  "${CTEST_SVN_COMMAND} co --username elastixguest --password elastixguest https://svn.bigr.nl/elastix/trunkpublic \"${CTEST_SOURCE_DIRECTORY}\"" )
-
-# Ctest should wipe the binary tree before running
-# to keep harddisk space usage from growing
-SET( CTEST_START_WITH_EMPTY_BINARY_DIRECTORY TRUE )
-
-# Build elastix in release mode
-SET( CTEST_BUILD_CONFIGURATION "Release" )
+  "${CTEST_SVN_COMMAND} co --username elastixguest --password elastixguest https://svn.bigr.nl/elastix/trunkpublic \"${CTEST_CHECKOUT_DIRECTORY}\"" )
 
 # The following does not seem to work:
 # find_program( CTEST_SVN_COMMAND NAMES svn )
 #SET( CTEST_SVN_COMMAND "C:/cygwin/bin/svn.exe" )
-#set( CTEST_UPDATE_COMMAND "${CTEST_SVN_COMMAND}" )
-#SET( $ENV{LC_MESSAGES}    "en_EN" )
+# should ctest wipe the binary tree before running
+SET( CTEST_START_WITH_EMPTY_BINARY_DIRECTORY TRUE )
 
 #SET( CTEST_BUILD_NAME "win7-64-vs2008" )
 #set( CTEST_CMAKE_GENERATOR "Visual Studio 9 2008 Win64" )
+
+set( CTEST_BUILD_CONFIGURATION "Release" )
 #set(WITH_KWSTYLE FALSE)
 #set(WITH_MEMCHECK FALSE)
 #set(WITH_COVERAGE FALSE)
 #set(WITH_DOCUMENTATION FALSE)
 
 # Usage of the initial cache seems to do the trick:
-# Configure the dashboard:
+# Configure the dashboard.
 set( CTEST_INITIAL_CACHE "
-// Specify build, generator:
-BUILDNAME:STRING=win7-64-vs2008
-CMAKE_GENERATOR:INTERNAL=Visual Studio 9 2008 Win64
+// Specify build, generator
+BUILDNAME:STRING=linux-64-gcc4.1.2
 CMAKE_CONFIGURATION_TYPES:STRING=Release
 
-// Specify svn command:
-SVNCOMMAND:FILEPATH=C:/cygwin/bin/svn.exe
+SVNCOMMAND:FILEPATH=/usr/bin/svn
 
-// Which ITK to use:
-ITK_DIR:PATH=D:/toolkits/ITK/3.20.0/bin
+// Which ITK to use
+ITK_DIR:PATH=/home/sklein/tk/itk/3.20/release
 
-// Some elastix settings, defining the configuration:
+// Some elastix settings, defining the configuration
 ELASTIX_BUILD_TESTING:BOOL=ON
 ELASTIX_ENABLE_PACKAGER:BOOL=ON
-ELASTIX_USE_CUDA:BOOL=ON
+ELASTIX_USE_CUDA:BOOL=OFF
+ELASTIX_USE_MEVISDICOMTIFF:BOOL=ON
 ELASTIX_IMAGE_DIMENSION:STRING=2;3;4
 ELASTIX_IMAGE_2D_PIXELTYPES:STRING=float
 ELASTIX_IMAGE_3D_PIXELTYPES:STRING=float
 ELASTIX_IMAGE_4D_PIXELTYPES:STRING=short
 
-// Compile all elastix components:
+// Compile all elastix components
 USE_AffineDTITransformElastix:BOOL=ON
 USE_BSplineInterpolatorFloat:BOOL=ON
 USE_BSplineResampleInterpolatorFloat:BOOL=ON
@@ -93,4 +87,5 @@ USE_NearestNeighborResampleInterpolator:BOOL=ON
 USE_RSGDEachParameterApart:BOOL=ON
 USE_ViolaWellsMutualInformationMetric:BOOL=ON
 ")
+
 
