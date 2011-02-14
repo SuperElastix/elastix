2ef033861732463705034ce1c9f5d3a5a26fbf61
Marius Staring <m.staring@lumc.nl>
2011-02-14 11:17:16 +0000

    MS:
    -ENH: better testing, for all spline orders, and for all parts of the domain

--- src/Testing/itkBSplineDerivativeKernelFunctionTest.cxx
+++ src/Testing/itkBSplineDerivativeKernelFunctionTest.cxx
@@ -13,6 +13,7 @@
 ======================================================================*/
 #include "itkBSplineDerivativeKernelFunction.h"
 #include "itkBSplineDerivativeKernelFunction2.h"
+#include "itkKernelFunction.h"
 
 #include <ctime>
 #include <iomanip>
@@ -21,25 +22,28 @@
 
 int main( int argc, char *argv[] )
 {
-  /** Some basic type definitions.
-   * NOTE: don't change the dimension or the spline order, since the
-   * hard-coded ground truth depends on this.
-   */
-  const unsigned int SplineOrder = 3;
+  /** Some basic type definitions. */
+  std::vector<unsigned int> splineOrders;
+  splineOrders.push_back( 1 );
+  splineOrders.push_back( 2 );
+  splineOrders.push_back( 3 );
 
-  //const double distance = 1e-3; // the allowable distance
-  //const double allowedTimeDifference = 0.1; // 10% is considered within limits
   /** The number of calls to Evaluate(). This number gives reasonably
    * fast test results in Release mode.
    */
-  unsigned int N = static_cast<unsigned int>( 1e8 );
+  unsigned int N = static_cast<unsigned int>( 1e7 );
+  const double maxAllowedDistance = 1e-5; // the allowable distance
 
   /** Other typedefs. */
-  typedef itk::BSplineDerivativeKernelFunction<SplineOrder> BSplineDerivativeKernelType;
-  typedef itk::BSplineDerivativeKernelFunction2<SplineOrder> BSplineDerivativeKernelType2;
+  typedef itk::KernelFunction                       BaseKernelType;
+  typedef itk::BSplineDerivativeKernelFunction<1>   KernelType_ITK_1;
+  typedef itk::BSplineDerivativeKernelFunction2<1>  KernelType_elx_1;
+  typedef itk::BSplineDerivativeKernelFunction<2>   KernelType_ITK_2;
+  typedef itk::BSplineDerivativeKernelFunction2<2>  KernelType_elx_2;
+  typedef itk::BSplineDerivativeKernelFunction<3>   KernelType_ITK_3;
+  typedef itk::BSplineDerivativeKernelFunction2<3>  KernelType_elx_3;
 
-  /** Create the kernel. */
-  BSplineDerivativeKernelType::Pointer dkernel = BSplineDerivativeKernelType::New();
+  /** Create the evaluation points. */
   const unsigned int size_u = 15;
   std::vector<double> u( size_u );
   u[ 0 ] = -2.5;
@@ -56,45 +60,76 @@ int main( int argc, char *argv[] )
     u[ i ] = -u[ -i + size_u + 1 ];
   }
 
-  /** Time the implementation. */
-  clock_t startClock = clock();
-  for ( unsigned int i = 0; i < N; ++i )
+  /** For all spline orders. */
+  for ( unsigned int so = 0; so < splineOrders.size(); so++ )
   {
-    dkernel->Evaluate( u[ 3 ] );
-  }
-  clock_t endClock = clock();
-  clock_t clockDiff = endClock - startClock;
-  std::cerr << "The elapsed time for ITK implementation is: "
-    << clockDiff << std::endl;
+    std::cerr << "Evaluating spline order " << splineOrders[ so ] << std::endl;
 
-  /** Create the kernel. */
-  BSplineDerivativeKernelType2::Pointer dkernel2 = BSplineDerivativeKernelType2::New();
-
-  /** Time the implementation. */
-  startClock = clock();
-  for ( unsigned int i = 0; i < N; ++i )
-  {
-    dkernel2->Evaluate( u[ 3 ] );
-  }
-  endClock = clock();
-  clockDiff = endClock - startClock;
-  std::cerr << "The elapsed time for our implementation is: "
-    << clockDiff << std::endl;
-
-  /***************************************************************************/
-
-  for ( unsigned int i = 0; i < size_u; ++i )
-  {
-    double diff = dkernel->Evaluate( u[ i ] ) - dkernel2->Evaluate( u[ i ] );
-    if ( diff > 1e-5 )
+    /** Create the kernel. */
+    BaseKernelType::Pointer kernel_ITK, kernel_elx;
+    if ( splineOrders[ so ] == 1 )
     {
-      std::cerr << "ERROR: our implementation differs from ITK." << std::endl;
+      kernel_ITK = KernelType_ITK_1::New();
+      kernel_elx = KernelType_elx_1::New();
+    }
+    else if ( splineOrders[ so ] == 2 )
+    {
+      kernel_ITK = KernelType_ITK_2::New();
+      kernel_elx = KernelType_elx_2::New();
+    }
+    else if ( splineOrders[ so ] == 3 )
+    {
+      kernel_ITK = KernelType_ITK_3::New();
+      kernel_elx = KernelType_elx_3::New();
+    }
+    else
+    {
+      std::cerr << "ERROR: spline order " << splineOrders[ so ]
+        << " not defined." << std::endl;
       return 1;
     }
-  }
-  std::cerr << "The results are good." << std::endl;
-  //std::cerr << "\nITK output: " << dkernel->Evaluate( u ) << std::endl;
-  //std::cerr << "Our output: " << dkernel2->Evaluate( u ) << std::endl;
+
+    /** Time the ITK implementation. */
+    clock_t startClock = clock();
+    for ( unsigned int i = 0; i < N; ++i )
+    {
+      for ( unsigned int j = 0; j < size_u; j++ )
+      {
+        kernel_ITK->Evaluate( u[ j ] );
+      }
+    }
+    clock_t endClock = clock();
+    clock_t clockDiff = endClock - startClock;
+    std::cerr << "The elapsed time for ITK implementation is: "
+      << clockDiff << " ms" << std::endl;
+
+    /** Time the elx implementation. */
+    startClock = clock();
+    for ( unsigned int i = 0; i < N; ++i )
+    {
+      for ( unsigned int j = 0; j < size_u; j++ )
+      {
+        kernel_elx->Evaluate( u[ j ] );
+      }
+    }
+    endClock = clock();
+    clockDiff = endClock - startClock;
+    std::cerr << "The elapsed time for the elastix implementation is: "
+      << clockDiff << " ms"  << std::endl;
+
+    /** Compare the results. */
+    for ( unsigned int i = 0; i < size_u; ++i )
+    {
+      double diff = kernel_ITK->Evaluate( u[ i ] ) - kernel_elx->Evaluate( u[ i ] );
+      if ( diff > maxAllowedDistance )
+      {
+        std::cerr << "ERROR: our implementation differs from ITK." << std::endl;
+        return 1;
+      }
+    }
+    std::cerr << "The results are good.\n" << std::endl;
+
+  } // end for all spline orders
 
   /** Return a value. */
   return 0;
