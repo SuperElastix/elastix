ca308f0a1a0169990ea9bcdc8f8580634686bc6a
Stefan Klein <s.klein@erasmusmc.nl>
2011-07-27 16:59:40 +0000

    -VER: elastix version -> 4.406

--- src/Core/Install/elxBaseComponent.h
+++ src/Core/Install/elxBaseComponent.h
@@ -37,7 +37,7 @@
 #include <sstream>
 
 /** The current elastix version. */
-#define __ELASTIX_VERSION 4.405
+#define __ELASTIX_VERSION 4.406
 
 /** All elastix components should be in namespace elastix. */
 namespace elastix
