b623faa7af2695d6e678fb501b33692de057e75c
Marius Staring <m.staring@lumc.nl>
2011-09-02 08:01:35 +0000

    ENH: updating elastix version to 4.5 in preparation of new release

--- src/Core/Install/elxBaseComponent.h
+++ src/Core/Install/elxBaseComponent.h
@@ -37,7 +37,7 @@
 #include <sstream>
 
 /** The current elastix version. */
-#define __ELASTIX_VERSION 4.406
+#define __ELASTIX_VERSION 4.500
 
 /** All elastix components should be in namespace elastix. */
 namespace elastix
