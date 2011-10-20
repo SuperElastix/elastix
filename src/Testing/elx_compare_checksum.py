c4b06e23dc6c9df165d2d3f771582362a7756d1f
Stefan Klein <s.klein@erasmusmc.nl>
2011-10-20 18:18:33 +0000

    SK:
    -ADD: python script to compare checksum

---
+++ src/Testing/elx_compare_checksum.py
@@ -0,0 +1,55 @@
+import sys
+import os
+import os.path
+from optparse import OptionParser
+
+#-------------------------------------------------------------------------------
+# the main function
+def main():
+    # usage, parse parameters
+    usage = "usage: %prog [options] arg"
+    parser = OptionParser( usage )
+
+    # option to debug and verbose
+    parser.add_option( "-v", "--verbose",
+        action="store_true", dest="verbose" )
+
+    # options to control files
+    parser.add_option( "-d", "--directory", dest="directory", help="elastix output directory" )
+    parser.add_option( "-b", "--baseline", dest="baseline", help="baseline checksum" )
+
+    (options, args) = parser.parse_args()
+
+    # Read elastix.log and find line with checksum
+    fileName = options.directory + "/" + "elastix.log"
+    f = open( fileName )    
+    for line in f:
+      if "Registration result checksum:" in line:
+        checksumline = line
+        break
+   
+
+    # Extract checksum
+    checksum = checksumline.split(': ')[1].rstrip( "\n" );
+
+    # Print result
+    print "The registration result checksum is: %s" % checksum
+    print "The baseline values is: %s" % options.baseline
+
+    if options.baseline != checksum:
+      print "FAILURE: These values are NOT the same.\n"
+      if options.verbose:
+        print "Complete elastix.log file:\n"
+        f.seek(0,0)
+        for line in f:
+          print "%s" % line,
+      f.close();
+      return 1
+
+    f.close();
+    print "SUCCESS: These values are the same."
+    return 0
+
+#-------------------------------------------------------------------------------
+if __name__ == '__main__':
+    sys.exit(main())
