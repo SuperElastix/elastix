0db7654592e710a11e3910add9240a938bc76bce
Marius Staring <m.staring@lumc.nl>
2011-11-16 14:33:37 +0000

    ENH: a convenience script to get a list of checksums from a series of directories. Usage example:
    python elx_get_checksum_list.py -l elastix_run_3DCT_lung.*

---
+++ src/Testing/elx_get_checksum_list.py
@@ -0,0 +1,57 @@
+import sys
+import os
+import os.path
+from optparse import OptionParser
+
+#-------------------------------------------------------------------------------
+# the main function
+def main():
+    # usage, parse parameters
+    usage = "usage: %prog [options] arg"
+    parser = OptionParser( usage )
+
+    # option to debug and verbose
+    parser.add_option( "-v", "--verbose",
+        action="store_true", dest="verbose" )
+
+    # options to control files
+    parser.add_option( "-l", "--list", type="string", dest="directoryList", help="list of elastix output directories" )
+    #parser.add_option( "-o", "--output", dest="output", help="output text file" )
+
+    (options, args) = parser.parse_args()
+
+    # Convert string -l to list
+    dirList = options.directoryList.split( ' ' )
+    # Add everything not processed
+    dirList.extend( args );
+    #print "dirList: %s" % dirList
+
+    print "directory checksum"
+
+    for directory in dirList:
+      # Equivalent to: fileName = options.directory + "/" + "elastix.log"
+      fileName = os.path.join( directory, "elastix.log" );
+
+      # Read elastix.log and find last line with checksum
+      try:
+        f = open( fileName )
+      except IOError as e:
+        print "%s No elastix.log found" % directory
+        continue
+
+      for line in f:
+        if "Registration result checksum:" in line:
+          checksumline = line
+
+      # Extract checksum
+      checksum = checksumline.split(': ')[1].rstrip( "\n" );
+
+      # Print result
+      print "%s %s" % (directory, checksum)
+      f.close();
+
+    return 0
+
+#-------------------------------------------------------------------------------
+if __name__ == '__main__':
+    sys.exit(main())
