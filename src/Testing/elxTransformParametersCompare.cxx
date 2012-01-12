d557791e78338bcc14ec288df44ceef4074b14f8
Marius Staring <m.staring@lumc.nl>
2012-01-12 12:36:42 +0000

    ENH: add the possibility to create a coefficient difference image in case of differences between the B-spline parameters.

--- src/Testing/elxTransformParametersCompare.cxx
+++ src/Testing/elxTransformParametersCompare.cxx
@@ -25,6 +25,9 @@
 #include "itkNumericTraits.h"
 #include "itkMath.h"
 #include "itksys/SystemTools.hxx"
+#include "itkImage.h"
+#include "itkImageRegionIterator.h"
+#include "itkImageFileWriter.h"
 
 
 /**
@@ -44,6 +47,9 @@ std::string GetHelpString( void )
 
 } // end GetHelpString()
 
+// This comparison works on all image types by reading images in a 6D double images. If images > 6 dimensions
+// must be compared, change this variable.
+static const unsigned int ITK_TEST_DIMENSION_MAX = 4;
 
 int main( int argc, char **argv )
 {
@@ -120,7 +126,7 @@ int main( int argc, char **argv )
   config->ReadParameter( parametersBaseline, "TransformParameters",
     0, numberOfParametersBaseline - 1, true, dummyErrorMessage );
 
-  // The sizes of the baseline and test parameters must match
+  /** The sizes of the baseline and test parameters must match. */
   std::cerr << "Baseline transform parameters: " << baselineFileName
     << " has " << numberOfParametersBaseline << " parameters." << std::endl;
   std::cerr << "Test transform parameters:     " << testFileName
@@ -132,7 +138,7 @@ int main( int argc, char **argv )
     return EXIT_FAILURE;
   }
 
-  // Now compare the two parameter vectors
+  /** Now compare the two parameter vectors. */
   ScalarType diffNorm = itk::NumericTraits<ScalarType>::Zero;
   for ( unsigned int i = 0; i < numberOfParametersTest; i++ )
   {
@@ -143,6 +149,104 @@ int main( int argc, char **argv )
   std::cerr << "The norm of the difference between baseline and test transform parameters was computed\n";
   std::cerr << "Computed difference: " << diffNorm << std::endl;
   std::cerr << "Allowed  difference: " << allowedTolerance << std::endl;
+
+  /** Check if this is a B-spline transform.
+   * If it is and if the difference is nonzero, we write a sort of coefficient
+   * difference image.
+   */
+  std::string transformName = "";
+  config->ReadParameter( transformName, "Transform", 0, true, dummyErrorMessage );
+  if( diffNorm > 1e-18 && transformName == "BSplineTransform" )
+  {
+    /** Get the true dimension. */
+    unsigned int dimension = 2;
+    config->ReadParameter( dimension, "FixedImageDimension", 0, true, dummyErrorMessage );
+
+    /** Typedefs. */
+    typedef itk::Image< float, ITK_TEST_DIMENSION_MAX >     CoefficientImageType;
+    typedef CoefficientImageType::RegionType                RegionType;
+    typedef RegionType::SizeType                            SizeType;
+    typedef RegionType::IndexType                           IndexType;
+    typedef CoefficientImageType::SpacingType               SpacingType;
+    typedef CoefficientImageType::PointType                 OriginType;
+    typedef CoefficientImageType::DirectionType             DirectionType;
+    typedef itk::ImageRegionIterator<CoefficientImageType>  IteratorType;
+    typedef itk::ImageFileWriter<CoefficientImageType>      WriterType;
+
+    /** Get coefficient image information. */
+    SizeType gridSize; gridSize.Fill( 1 );
+    IndexType gridIndex; gridIndex.Fill( 0 );
+    SpacingType gridSpacing; gridSpacing.Fill( 1.0 );
+    OriginType gridOrigin; gridOrigin.Fill( 0.0 );
+    DirectionType gridDirection; gridDirection.SetIdentity();
+    for ( unsigned int i = 0; i < dimension; i++ )
+    {
+      config->ReadParameter( gridSize[ i ], "GridSize", i, true, dummyErrorMessage );
+      config->ReadParameter( gridIndex[ i ], "GridIndex", i, true, dummyErrorMessage );
+      config->ReadParameter( gridSpacing[ i ], "GridSpacing", i, true, dummyErrorMessage );
+      config->ReadParameter( gridOrigin[ i ], "GridOrigin", i, true, dummyErrorMessage );
+      for ( unsigned int j = 0; j < dimension; j++ )
+      {
+        config->ReadParameter( gridDirection( j, i),
+          "GridDirection", i * dimension + j, true, dummyErrorMessage );
+      }
+    }
+
+    /** Create the coefficient image. */
+    CoefficientImageType::Pointer coefImage = CoefficientImageType::New();
+    RegionType region; region.SetSize( gridSize ); region.SetIndex( gridIndex );
+    coefImage->SetRegions( region );
+    coefImage->SetSpacing( gridSpacing );
+    coefImage->SetOrigin( gridOrigin );
+    coefImage->SetDirection( gridDirection );
+    coefImage->Allocate();
+
+    /** Fill the coefficient image with the difference of the B-spline
+     * parameters. Since there are dimension number of differences,
+     * we take the norm.
+     */
+    IteratorType it( coefImage, coefImage->GetLargestPossibleRegion() );
+    it.GoToBegin();
+    unsigned int index = 0;
+    const unsigned int numberParPerDim = numberOfParametersTest / dimension;
+    while( !it.IsAtEnd() )
+    {
+      ScalarType diffNorm = itk::NumericTraits<ScalarType>::Zero;
+      for ( unsigned int i = 0; i < dimension; i++ )
+      {
+        unsigned int j = index + i * numberParPerDim;
+        diffNorm += vnl_math_sqr( parametersBaseline[ j ] - parametersTest[ j ] );
+      }
+      diffNorm = vcl_sqrt( diffNorm );
+
+      it.Set( diffNorm );
+      ++it; index++;
+    }
+
+    /** Create name for difference image. */
+    std::string diffImageFileName =
+      itksys::SystemTools::GetFilenamePath( testFileName );
+    diffImageFileName += "/";
+    diffImageFileName +=
+      itksys::SystemTools::GetFilenameWithoutLastExtension( testFileName );
+    diffImageFileName += "_DIFF_PAR.mha";
+
+    /** Write the difference image. */
+    WriterType::Pointer writer = WriterType::New();
+    writer->SetFileName( diffImageFileName );
+    writer->SetInput( coefImage );
+    try
+    {
+      writer->Write();
+    }
+    catch ( itk::ExceptionObject & err )
+    {
+      std::cerr << "Error during writing difference image: " << err << std::endl;
+      return EXIT_FAILURE;
+    }
+  }
+
+  /** Return. */
   if( diffNorm > allowedTolerance )
   {
     std::cerr << "ERROR: The difference is larger than acceptable!" << std::endl;
