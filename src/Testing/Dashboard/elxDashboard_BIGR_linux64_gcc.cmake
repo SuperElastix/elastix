6b3b7d3b35f80437641e6a98961e97bfc845000e
Stefan Klein <s.klein@erasmusmc.nl>
2012-02-12 17:09:43 +0000

    SK:
    -ENH: update dashboard script to new cluster. The old cluster will be gone soon.

--- src/Testing/Dashboard/elxDashboard_BIGR_linux64_gcc.cmake
+++ src/Testing/Dashboard/elxDashboard_BIGR_linux64_gcc.cmake
@@ -12,13 +12,13 @@
 # NOTE that Model should directly follow the comma: no space allowed!
 #
 # Setup: linux 64bit
-# gcc 4.1.2 20061115 (prerelease) (SUSE Linux),
+# gcc 4.4.6 (Redhat Linux),
 # Release mode, ITK 4.1rc01
-# PC: linux cluster at BIGR (SK).
+# PC: linux cluster2 at BIGR (SK).
 
 # Client maintainer: s.klein@erasmusmc.nl
 set( CTEST_SITE "BIGR.cluster" )
-set( CTEST_BUILD_NAME "Linux-64bit-gcc4.1.2" )
+set( CTEST_BUILD_NAME "Linux-64bit-gcc4.4.6" )
 set( CTEST_BUILD_CONFIGURATION Release )
 set( CTEST_CMAKE_GENERATOR "Unix Makefiles" )
 
@@ -28,24 +28,24 @@ set( CTEST_CMAKE_GENERATOR "Unix Makefiles" )
 # Specify the kind of dashboard to submit
 # default: Nightly
 SET( dashboard_model Nightly )
+SET( CTEST_BUILD_FLAGS "-j3" ) # parallel build for makefiles
+SET( CTEST_TEST_ARGS PARALLEL_LEVEL 3 ) # parallel testing
+
 IF( ${CTEST_SCRIPT_ARG} MATCHES Experimental )
   SET( dashboard_model Experimental )
-  # speed up the experimental build a bit:
-  set( CTEST_BUILD_FLAGS "-j2" ) # parallel build for makefiles
-  set( CTEST_TEST_ARGS PARALLEL_LEVEL 2 ) # parallel testing
 ELSEIF( ${CTEST_SCRIPT_ARG} MATCHES Continuous )
   SET( dashboard_model Continuous )
 ENDIF()
 
 # Output directory
-set( CTEST_DASHBOARD_ROOT "/home/sklein/tk/mydash/${CTEST_SCRIPT_NAME}.${dashboard_model}" )
+set( CTEST_DASHBOARD_ROOT "/cm/shared/apps/elastix/nightly" )
 
 #set(dashboard_do_memcheck 1)
 #set(dashboard_do_coverage 1)
 
 SET( dashboard_cache "
 // Which ITK to use
-ITK_DIR:PATH=/home/sklein/tk/itk/4.1rc01/release
+ITK_DIR:PATH=/cm/shared/apps/itk/4.1rc01/release
 
 // Some elastix settings, defining the configuration
 ELASTIX_BUILD_TESTING:BOOL=ON
@@ -54,8 +54,11 @@ ELASTIX_USE_CUDA:BOOL=OFF
 ELASTIX_USE_MEVISDICOMTIFF:BOOL=ON
 ELASTIX_IMAGE_DIMENSION:STRING=2;3;4
 ELASTIX_IMAGE_2D_PIXELTYPES:STRING=float
-ELASTIX_IMAGE_3D_PIXELTYPES:STRING=float
+ELASTIX_IMAGE_3D_PIXELTYPES:STRING=short;float
 ELASTIX_IMAGE_4D_PIXELTYPES:STRING=short
+
+// Install dir
+CMAKE_INSTALL_PREFIX:PATH=/cm/shared/apps/elastix/nightly/install
 
 // Compile all elastix components;
 USE_ALL_COMPONENTS:BOOL=ON
