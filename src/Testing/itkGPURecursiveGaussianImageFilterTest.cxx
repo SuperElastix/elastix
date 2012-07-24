29c3c01411ef73479e1376e94d7ec5cfd17b2351
Marius Staring <m.staring@lumc.nl>
2012-07-24 15:30:02 +0000

    ENH: Cleanup of OpenCL test GPURecursiveGaussianImageFilterTest
    - Pragma push and pop not needed in elastix. This is taken care of via CMake.
    - Print results in table
    - Refactoring using helper function to compute RMSE
    - Removed extra 1D test
    - Using vnl function everywhere
    - Return EXIT_FAILURE when RMSE too large

--- src/Testing/itkGPURecursiveGaussianImageFilterTest.cxx
+++ src/Testing/itkGPURecursiveGaussianImageFilterTest.cxx
@@ -11,43 +11,53 @@
      PURPOSE. See the above copyright notices for more information.
 
 ======================================================================*/
-#pragma warning(push)
-// warning C4996: 'std::copy': Function call with parameters that may be unsafe
-// - this call relies on the caller to check that the passed values are correct.
-// To disable this warning, use -D_SCL_SECURE_NO_WARNINGS. See documentation on
-// how to use Visual C++ 'Checked Iterators'
-#pragma warning(disable:4996)
-#include "itkImageFileReader.h"
-#include "itkImageFileWriter.h"
-#pragma warning(pop)
 
-#include "itkTimeProbe.h"
 #include "itkGPURecursiveGaussianImageFilter.h"
 
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkTimeProbe.h"
 #include "itkOpenCLUtil.h" // IsGPUAvailable()
+#include <iomanip> // setprecision, etc.
+
+
+// Helper function
+template<class ImageType>
+double ComputeRMSE( ImageType * cpuImage, ImageType * gpuImage )
+{
+  itk::ImageRegionIterator<ImageType> cit(
+    cpuImage, cpuImage->GetLargestPossibleRegion() );
+  itk::ImageRegionIterator<ImageType> git(
+    gpuImage, gpuImage->GetLargestPossibleRegion() );
+
+  double rmse = 0.0;
+  for( cit.GoToBegin(), git.GoToBegin(); !cit.IsAtEnd(); ++cit, ++git )
+  {
+    double err = static_cast<double>( cit.Get() ) - static_cast<double>( git.Get() );
+    rmse += err * err;
+  }
+  rmse = vcl_sqrt( rmse / cpuImage->GetLargestPossibleRegion().GetNumberOfPixels() );
+  return rmse;
+} // end ComputeRMSE()
+
 
 /**
-* Testing GPU Recursive Gaussian Image Filter
-*/
-#define ImageDimension 3 // 2
+ * Testing GPU Recursive Gaussian Image Filter
+ */
 
-// 3D:
-// D:\\work\\elastix-ext\\ITK4OpenCL\\data\\image-512x512x107-3D.mha D:\\work\\elastix-ext\\ITK4OpenCL\\data\\image-512x512x107-3D-out.mha
-// 3D:
-// D:\\work\\elastix-ext\\ITK4OpenCL\\data\\image-48x62x42-3D.mha D:\\work\\elastix-ext\\ITK4OpenCL\\data\\image-48x62x42-3D-out.mha
-int main(int argc, char *argv[])
+int main( int argc, char * argv[] )
 {
   // register object factory for GPU image and filter
   //itk::ObjectFactoryBase::RegisterFactory( itk::GPUImageFactory::New() );
   //itk::ObjectFactoryBase::RegisterFactory( itk::GPURecursiveGaussianImageFilterFactory::New() );
 
+  const unsigned int ImageDimension = 3; // 2
   typedef float InputPixelType;
   typedef float OutputPixelType;
-
   typedef itk::GPUImage<InputPixelType,  ImageDimension>  InputImageType;
   typedef itk::GPUImage<OutputPixelType, ImageDimension>  OutputImageType;
 
-  typedef itk::RecursiveGaussianImageFilter<InputImageType, OutputImageType> CPUFilterType;
+  typedef itk::RecursiveGaussianImageFilter<InputImageType, OutputImageType>    CPUFilterType;
   typedef itk::GPURecursiveGaussianImageFilter<InputImageType, OutputImageType> GPUFilterType;
 
   typedef itk::ImageFileReader<InputImageType>   ReaderType;
@@ -55,24 +65,27 @@ int main(int argc, char *argv[])
 
   if( argc <  3 )
   {
-    std::cerr << "Error: missing arguments" << std::endl;
-    std::cerr << "inputfile outputfile " << std::endl;
+    std::cerr << "ERROR: missing arguments" << std::endl;
+    std::cerr << "  inputfile outputfile " << std::endl;
     return EXIT_FAILURE;
   }
 
-  if(!itk::IsGPUAvailable())
+  if( !itk::IsGPUAvailable() )
   {
     std::cerr << "OpenCL-enabled GPU is not present." << std::endl;
     return EXIT_FAILURE;
   }
 
+  // Some hard-coded testing options
   const double sigma = 3.0;
   unsigned int direction = 0;
   const double epsilon = 0.01;
-  const unsigned int maximumNumberOfThreads = itk::MultiThreader::GetGlobalDefaultNumberOfThreads();
+  const unsigned int maximumNumberOfThreads
+    = itk::MultiThreader::GetGlobalDefaultNumberOfThreads();
   const bool testspeed      = true;
   const bool testdirections = true; // Works for 48x62x42 image, Does not work for 512x512x107 ! Why?
-  const bool test1D         = true;
+
+  std::cout << std::showpoint << std::setprecision( 4 );
 
   // Reader
   ReaderType::Pointer reader = ReaderType::New();
@@ -81,258 +94,120 @@ int main(int argc, char *argv[])
   {
     reader->Update();
   }
-  catch( itk::ExceptionObject & itkNotUsed( excp ) )
+  catch( itk::ExceptionObject & excp )
   {
-    //ML_PRINT_ERROR( "Execute()", ML_UNKNOWN_EXCEPTION, excp.GetDescription() );
-    //return;
+    std::cerr << "ERROR: " << excp << std::endl;
+    return EXIT_FAILURE;
   }
+
+  CPUFilterType::Pointer CPUFilter = CPUFilterType::New();
+  GPUFilterType::Pointer GPUFilter = GPUFilterType::New();
+  itk::TimeProbe cputimer;
+  itk::TimeProbe gputimer;
 
   // Test 1~n threads for CPU
   // Speed CPU vs GPU
-  if(testspeed)
+  if( testspeed )
   {
-    std::cout << "Testing 1~"<< maximumNumberOfThreads <<" threads for CPU vs GPU:\n";
+    std::cout << "Testing the Recursive Gaussian filter, CPU vs GPU:\n";
+    std::cout << "CPU/GPU sigma direction #threads RMSE\n";
 
-    for(unsigned int nThreads = 1; nThreads <= maximumNumberOfThreads; nThreads++)
+    for( unsigned int nThreads = 1; nThreads <= maximumNumberOfThreads; nThreads++ )
     {
       // Test CPU
-      CPUFilterType::Pointer CPUFilter = CPUFilterType::New();
-
-      itk::TimeProbe cputimer;
       cputimer.Start();
-
       CPUFilter->SetNumberOfThreads( nThreads );
-
       CPUFilter->SetInput( reader->GetOutput() );
       CPUFilter->SetSigma( sigma );
       CPUFilter->SetDirection( direction );
       CPUFilter->Update();
-
       cputimer.Stop();
-      std::cout << "CPU Recursive Gaussian filter took " << cputimer.GetMean() << " seconds with "
-        << CPUFilter->GetNumberOfThreads() << " threads."
-        << " For direction: "<< direction <<", sigma: "<< sigma << std::endl;
 
-      // Test GPU
-      if( nThreads == maximumNumberOfThreads )
-      {
-        GPUFilterType::Pointer GPUFilter = GPUFilterType::New();
-
-        itk::TimeProbe gputimer;
-        gputimer.Start();
-
-        GPUFilter->SetInput( reader->GetOutput() );
-        GPUFilter->SetSigma( sigma );
-        GPUFilter->SetDirection( direction );
-        GPUFilter->Update();
-
-        // Commented out, not in the GPU-Alpha branch
-        //GPUFilter->GetOutput()->UpdateBuffers(); // synchronization point (GPU->CPU memcpy)
-
-        gputimer.Stop();
-        std::cout << "GPU Recursive Gaussian filter took " << gputimer.GetMean() << " seconds."
-          << " For direction: "<< direction <<", sigma: "<< sigma << std::endl;
-
-        // RMS Error check
-        double diff = 0.0;
-        unsigned int nPix = 0;
-        itk::ImageRegionIterator<OutputImageType> cit(CPUFilter->GetOutput(), CPUFilter->GetOutput()->GetLargestPossibleRegion());
-        itk::ImageRegionIterator<OutputImageType> git(GPUFilter->GetOutput(), GPUFilter->GetOutput()->GetLargestPossibleRegion());
-
-        for(cit.GoToBegin(), git.GoToBegin(); !cit.IsAtEnd(); ++cit, ++git)
-        {
-          double err = vnl_math_abs((double)(cit.Get()) - (double)(git.Get()));
-          if(err > epsilon)
-            std::cout << "CPU : " << (double)(cit.Get()) << ", GPU : " << (double)(git.Get()) << std::endl;
-          diff += err*err;
-          nPix++;
-        }
-
-        std::cout << "RMS Error: " << sqrt( diff / (double)nPix ) << std::endl;
-
-        // Write output image
-        //WriterType::Pointer writer = WriterType::New();
-        //writer->SetInput(GPUFilter->GetOutput());
-        //writer->SetFileName( argv[2] );
-        //writer->Update();
-      }
+      std::cout << "CPU " << sigma << " " << direction << " " << nThreads
+        << " " << cputimer.GetMean() << std::endl;
     }
-  }
+
+    // Test GPU
+    gputimer.Start();
+    GPUFilter->SetInput( reader->GetOutput() );
+    GPUFilter->SetSigma( sigma );
+    GPUFilter->SetDirection( direction );
+    GPUFilter->Update();
+    gputimer.Stop();
+
+    std::cout << "GPU " << sigma << " " << direction << " x "
+      << gputimer.GetMean();
+
+    double rmse = ComputeRMSE<OutputImageType>( CPUFilter->GetOutput(), GPUFilter->GetOutput() );
+    std::cout << " " << rmse << std::endl;
+
+    // Write output image
+    WriterType::Pointer writer = WriterType::New();
+    writer->SetInput( GPUFilter->GetOutput() );
+    writer->SetFileName( argv[2] );
+    try
+    {
+      writer->Update();
+    }
+    catch( itk::ExceptionObject & excp )
+    {
+      std::cerr << "ERROR: " << excp << std::endl;
+      return EXIT_FAILURE;
+    }
+
+    // Check
+    if ( rmse > epsilon )
+    {
+      std::cerr << "ERROR: RMSE between CPU and GPU result larger than expected" << std::endl;
+      return EXIT_FAILURE;
+    }
+  } // end if testing over resolutions
 
   // Works for 48x62x42 image, Does not work for 512x512x107 ! Why?
-  if(testdirections)
+  if( testdirections )
   {
-    std::cout << "Testing directions switch CPU vs GPU:\n";
+    std::cout << "\n\nTesting directions switch, CPU vs GPU:\n";
+    std::cout << "CPU/GPU sigma direction #threads RMSE\n";
 
     // Check directions
-    for(direction=0; direction<ImageDimension; direction++)
+    for( direction = 0; direction < ImageDimension; direction++ )
     {
-      // Test CPU
-      CPUFilterType::Pointer CPUFilter = CPUFilterType::New();
-
-      itk::TimeProbe cputimer;
       cputimer.Start();
-
       CPUFilter->SetNumberOfThreads( maximumNumberOfThreads );
       CPUFilter->SetInput( reader->GetOutput() );
       CPUFilter->SetSigma( sigma );
       CPUFilter->SetDirection( direction );
       CPUFilter->Modified();
       CPUFilter->Update();
-
       cputimer.Stop();
-      std::cout << "CPU Recursive Gaussian filter took " << cputimer.GetMean() << " seconds with "
-        << CPUFilter->GetNumberOfThreads() << " threads."
-        << " For direction: "<< direction <<", sigma: "<< sigma << std::endl;
+
+      std::cout << "CPU " << sigma << " " << direction << " " << maximumNumberOfThreads
+        << " " << cputimer.GetMean() << std::endl;
 
       // Test GPU
-      GPUFilterType::Pointer GPUFilter = GPUFilterType::New();
-
-      itk::TimeProbe gputimer;
       gputimer.Start();
-
       GPUFilter->SetInput( reader->GetOutput() );
       GPUFilter->SetSigma( sigma );
       GPUFilter->SetDirection( direction );
       GPUFilter->Modified();
       GPUFilter->Update();
-
-      // Commented out, not in the GPU-Alpha branch
-      //GPUFilter->GetOutput()->UpdateBuffers(); // synchronization point (GPU->CPU memcpy)
-
       gputimer.Stop();
-      std::cout << "GPU Recursive Gaussian filter took " << gputimer.GetMean() << " seconds."
-        << " For direction: "<< direction <<", sigma: "<< sigma << std::endl;
 
-      // RMS Error check
-      double diff = 0.0;
-      unsigned int nPix = 0;
-      itk::ImageRegionIterator<OutputImageType> cit(CPUFilter->GetOutput(), CPUFilter->GetOutput()->GetLargestPossibleRegion());
-      itk::ImageRegionIterator<OutputImageType> git(GPUFilter->GetOutput(), GPUFilter->GetOutput()->GetLargestPossibleRegion());
+      std::cout << "GPU " << sigma << " " << direction << " x "
+      << gputimer.GetMean();
 
-      for(cit.GoToBegin(), git.GoToBegin(); !cit.IsAtEnd(); ++cit, ++git)
+      double rmse = ComputeRMSE<OutputImageType>( CPUFilter->GetOutput(), GPUFilter->GetOutput() );
+      std::cout << " " << rmse << std::endl;
+
+      // Check
+      if ( rmse > epsilon )
       {
-        double err = vnl_math_abs((double)(cit.Get()) - (double)(git.Get()));
-        if(err > epsilon)
-          std::cout << "CPU : " << (double)(cit.Get()) << ", GPU : " << (double)(git.Get()) << std::endl;
-        diff += err*err;
-        nPix++;
+        std::cerr << "ERROR: RMSE between CPU and GPU result larger than expected" << std::endl;
+        return EXIT_FAILURE;
       }
-
-      std::cout << "RMS Error for direction" << direction << " : " << sqrt( diff / (double)nPix ) << std::endl;
     }
-  }
-
-  // Test 1D
-  if(test1D)
-  {
-    std::cout << "Testing 1D CPU vs GPU:\n";
-
-    typedef itk::Image<InputPixelType, 1> ImageType1D;
-    typedef ImageType1D::SizeType         SizeType;
-    typedef ImageType1D::IndexType        IndexType;
-    typedef ImageType1D::RegionType       RegionType;
-    typedef ImageType1D::SpacingType      SpacingType;
-
-    typedef itk::NumericTraits<InputPixelType>::RealType PixelRealType;
-
-    SizeType size;
-    size[0] = 513;
-
-    IndexType start;
-    start[0] = 0;
-
-    RegionType region;
-    region.SetIndex( start );
-    region.SetSize( size );
-
-    SpacingType spacing;
-    spacing[0] = 1.0;
-
-    // CPU Image
-    ImageType1D::Pointer CPUInputImage = ImageType1D::New();
-    CPUInputImage->SetRegions( region );
-    CPUInputImage->Allocate();
-    CPUInputImage->SetSpacing( spacing );
-    CPUInputImage->FillBuffer( itk::NumericTraits<InputPixelType>::Zero );
-
-    IndexType index0;
-    index0[0] = ( size[0] - 1 ) / 2; // the middle pixel
-    CPUInputImage->SetPixel( index0, static_cast<InputPixelType>( 1000.0 ) );
-
-    // CPU filter
-    typedef itk::RecursiveGaussianImageFilter<ImageType1D, ImageType1D> CPUFilterType1D;
-    CPUFilterType1D::Pointer CPUFilter = CPUFilterType1D::New();
-    CPUFilter->SetInput( CPUInputImage );
-    CPUFilter->SetNormalizeAcrossScale( true );
-    CPUFilter->SetSigma( sigma );
-    CPUFilter->Update();
-
-    // GPU Image
-    itk::ObjectFactoryBase::RegisterFactory( itk::GPUImageFactory::New() );
-
-    ImageType1D::Pointer GPUInputImage = ImageType1D::New();
-    GPUInputImage->SetRegions( region );
-    GPUInputImage->Allocate();
-    GPUInputImage->SetSpacing( spacing );
-    GPUInputImage->FillBuffer( itk::NumericTraits<InputPixelType>::Zero );
-
-    IndexType index1;
-    index1[0] = ( size[0] - 1 ) / 2;  // the middle pixel
-    GPUInputImage->SetPixel( index1, static_cast<InputPixelType>( 1000.0 ) );
-
-    // Check input images
-    double diff = 0.0;
-    unsigned int nPix = 0;
-    itk::ImageRegionIterator<ImageType1D> cinIt(CPUInputImage, CPUInputImage->GetLargestPossibleRegion());
-    itk::ImageRegionIterator<ImageType1D> ginIt(GPUInputImage, GPUInputImage->GetLargestPossibleRegion());
-
-    std::cout <<"Checking input images...";
-
-    for(cinIt.GoToBegin(), ginIt.GoToBegin(); !cinIt.IsAtEnd(); ++cinIt, ++ginIt)
-    {
-      double err = vnl_math_abs((double)(cinIt.Get()) - (double)(ginIt.Get()));
-      if(err > epsilon)
-      {
-        const IndexType cindex = cinIt.GetIndex();
-        std::cout <<"Index : "<< cindex << " CPU : " << (double)(cinIt.Get()) << ", GPU : " << (double)(ginIt.Get()) << std::endl;
-      }
-      diff += err*err;
-      nPix++;
-    }
-    if(diff == 0)
-      std::cout <<" Ok." << std::endl;
-    else
-      std::cout <<" Fail." << std::endl;
-
-    // GPU filter
-    typedef itk::GPURecursiveGaussianImageFilter<ImageType1D, ImageType1D> GPUFilterType1D;
-    GPUFilterType1D::Pointer GPUFilter = GPUFilterType1D::New();
-    GPUFilter->SetInput( GPUInputImage );
-    GPUFilter->SetNormalizeAcrossScale( true );
-    GPUFilter->SetSigma( sigma );
-    GPUFilter->Update();
-
-    // RMS Error check
-    diff = 0.0;
-    nPix = 0;
-    itk::ImageRegionIterator<ImageType1D> cit(CPUFilter->GetOutput(), CPUFilter->GetOutput()->GetLargestPossibleRegion());
-    itk::ImageRegionIterator<ImageType1D> git(GPUFilter->GetOutput(), GPUFilter->GetOutput()->GetLargestPossibleRegion());
-
-    for(cit.GoToBegin(), git.GoToBegin(); !cit.IsAtEnd(); ++cit, ++git)
-    {
-      double err = vnl_math_abs((double)(cit.Get()) - (double)(git.Get()));
-      if(err > epsilon)
-      {
-        const IndexType cindex = cit.GetIndex();
-        std::cout <<"Index : "<< cindex << " CPU : " << (double)(cit.Get()) << ", GPU : " << (double)(git.Get()) << std::endl;
-      }
-      diff += err*err;
-      nPix++;
-    }
-
-    std::cout << "RMS Error for 1D test: " << sqrt( diff / (double)nPix ) << std::endl;
   }
 
   return EXIT_SUCCESS;
-}
+
+} // end main()
