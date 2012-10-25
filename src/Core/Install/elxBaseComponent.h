53eb3a1aad21aa79d851ee1b5cb6ccdab68aae29
Marius Staring <m.staring@lumc.nl>
2012-10-25 09:17:34 +0000

    ENH: increased elastix version to 4.601

--- src/Core/Install/elxBaseComponent.h
+++ src/Core/Install/elxBaseComponent.h
@@ -37,7 +37,7 @@
 #include <sstream>
 
 /** The current elastix version. */
-#define __ELASTIX_VERSION 4.600
+#define __ELASTIX_VERSION 4.601
 
 /** All elastix components should be in namespace elastix. */
 namespace elastix
