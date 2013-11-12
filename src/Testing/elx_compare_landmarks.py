4690944e6aa08cd58861b45e0f4dec7890869564
Marius Staring <m.staring@lumc.nl>
2013-11-12 14:19:56 +0000

    ENH: adding a test possibility for comparing landmark errors

---
+++ src/Testing/elx_compare_landmarks.py
@@ -0,0 +1,102 @@
+import sys, subprocess
+import os
+import os.path
+import re
+import shutil
+import math
+#import numpy
+from optparse import OptionParser
+
+#-------------------------------------------------------------------------------
+# the main function
+def main():
+  # usage, parse parameters
+  usage = "usage: %prog [options] arg";
+  parser = OptionParser( usage );
+
+  # option to debug and verbose
+  parser.add_option( "-v", "--verbose", action="store_true", dest="verbose" );
+
+  # options to control files
+  parser.add_option( "-d", "--directory", dest="directory", help="elastix output directory" );
+  parser.add_option( "-f", "--fixedlandmarks", dest="flm", help="fixed image landmarks" );
+  parser.add_option( "-b", "--baselinetp", dest="btp", help="baseline transform parameter file" );
+
+  (options, args) = parser.parse_args();
+
+  # Check if option -d and -f and -b are given
+  if options.directory == None :
+    parser.error( "The option directory (-d) should be given" );
+  if options.flm == None :
+    parser.error( "The option directory (-f) should be given" );
+  if options.btp == None :
+    parser.error( "The option directory (-b) should be given" );
+
+  # Get the moving image mask
+
+  # Get the transform parameters files
+  tpFileName   = os.path.join( options.directory, "TransformParameters.0.txt" );
+  tpFileName_b = options.btp;
+
+  # Sanity checks
+  if not os.path.exists( tpFileName ) :
+    print( "ERROR: the file " + tpFileName + " does not exist" );
+    return 1;
+
+  #
+  # Transform the fixed image landmarks by the current result
+  #
+  print( "Transforming fixed image landmarks using " + tpFileName );
+  landmarks1 = os.path.join( options.directory, "landmarks_current.txt" );
+  subprocess.call( [ "transformix", "-def", options.flm, "-out", options.directory, "-tp", tpFileName ],
+    stdout=subprocess.PIPE );
+
+  # Parse file to extract only the column with the output points
+  f1 = open( os.path.join( options.directory, "outputpoints.txt" ), 'r' );
+  f2 = open( landmarks1, 'w' );
+  for line in f1 :
+    f2.write( line.strip().split(';')[4].strip().strip( "OutputPoint = [ " ).rstrip( " ]" ) + "\n" );
+  f1.close(); f2.close();
+
+  #
+  # Transform the fixed image landmarks by the baseline result
+  #
+  print( "Transforming fixed image landmarks using " + tpFileName_b );
+  landmarks2 = os.path.join( options.directory, "landmarks_baseline.txt" );
+  subprocess.call( [ "transformix", "-def", options.flm, "-out", options.directory, "-tp", tpFileName_b ],
+    stdout=subprocess.PIPE );
+  shutil.copyfile( os.path.join( options.directory, "outputpoints.txt" ), landmarks2 );
+
+  # Parse file to extract only the column with the output points
+  f1 = open( os.path.join( options.directory, "outputpoints.txt" ), 'r' );
+  f2 = open( landmarks2, 'w' );
+  for line in f1 :
+    f2.write( line.strip().split(';')[4].strip().strip( "OutputPoint = [ " ).rstrip( " ]" ) + "\n" );
+  f1.close(); f2.close();
+
+  # Compute the distance between transformed landmarks
+  f1 = open( landmarks1, 'r' ); f2 = open( landmarks2, 'r' ); distance = 0.0;
+  for line1, line2 in zip( f1, f2 ) :
+    floats1 = [ float(x) for x in line1.split() ];
+    floats2 = [ float(x) for x in line2.split() ];
+    diffSquared = [ (m - n) * (m - n) for m, n in zip( floats1, floats2 ) ];
+    distance = distance + math.sqrt( sum( diffSquared ) );
+  meandistance = distance / 100.0;
+
+  # With numpy it would be:
+  #l1 = numpy.loadtxt( landmarks1 );
+  #l2 = numpy.loadtxt( landmarks2 );
+  #meandistance = numpy.mean( numpy.sum( (l1-l2)**2, axis=-1)**0.5 );
+
+  # Report
+  print( "The mean landmark distance between current and baseline is " + "{0:.3f}".format( meandistance ) + " mm" );
+  if meandistance < 1.0 :
+    print( "SUCCESS: mean landmark distance is lower than 1.0 mm" );
+    return 0;
+  else :
+    print( "FAILURE: mean landmark distance is higher than 1.0 mm" );
+    return 1;
+
+#-------------------------------------------------------------------------------
+if __name__ == '__main__':
+    sys.exit(main())
