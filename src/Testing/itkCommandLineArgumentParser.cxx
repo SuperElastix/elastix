34ecdf6bba2fdf1daf2948663c08c8bda5eb76a5
Marius Staring <m.staring@lumc.nl>
2014-05-07 09:12:26 +0000

    DOC: add missing copyright notice

--- src/Testing/itkCommandLineArgumentParser.cxx
+++ src/Testing/itkCommandLineArgumentParser.cxx
@@ -1,5 +1,16 @@
-#ifndef __itkCommandLineArgumentParser_cxx
-#define __itkCommandLineArgumentParser_cxx
+/*======================================================================
+
+  This file is part of the elastix software.
+
+  Copyright (c) University Medical Center Utrecht. All rights reserved.
+  See src/CopyrightElastix.txt or http://elastix.isi.uu.nl/legal.php for
+  details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE. See the above copyright notices for more information.
+
+======================================================================*/
 
 #include "itkCommandLineArgumentParser.h"
 
@@ -307,5 +318,3 @@ CommandLineArgumentParser
 
 
 } // end namespace itk
-
-#endif // end #ifndef __itkCommandLineArgumentParser_h
