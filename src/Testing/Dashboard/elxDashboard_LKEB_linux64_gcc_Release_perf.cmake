26dab007a7fd33e641b318b19693fb890e8476db
Marius Staring <m.staring@lumc.nl>
2014-05-21 10:50:12 +0000

    ENH: updated dashboard names
    after goliath updated gcc to 4.8.2

--- src/Testing/Dashboard/elxDashboard_LKEB_linux64_gcc_Release_perf.cmake
+++ src/Testing/Dashboard/elxDashboard_LKEB_linux64_gcc_Release_perf.cmake
@@ -11,14 +11,14 @@
 # and defaults to Nightly.
 # NOTE that Model should directly follow the comma: no space allowed!
 #
-# Setup: Linux 64bit, Ubuntu 3.2.0-34-generic
-# gcc 4.6.3
+# Setup: Linux 64bit, Ubuntu 14.04 LTS, 3.13.0-24-generic
+# gcc 4.8.2
 # Release mode, ITK 4.x (git)
 # PC: LKEB (MS), goliath
 
 # Client maintainer: m.staring@lumc.nl
 set( CTEST_SITE "LKEB.goliath" )
-set( CTEST_BUILD_NAME "Linux-64bit-gcc4.6.3-Release-perf" )
+set( CTEST_BUILD_NAME "Linux-64bit-gcc4.8.2-Release-perf" )
 set( CTEST_BUILD_FLAGS "-j6" ) # parallel build for makefiles
 set( CTEST_TEST_ARGS PARALLEL_LEVEL 6 ) # parallel testing
 set( CTEST_BUILD_CONFIGURATION Release )
