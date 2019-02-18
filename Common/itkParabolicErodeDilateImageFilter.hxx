6fbd4b4b747c7533d072fca0589b07b25c830d03
Niels Dekker <N.Dekker@lumc.nl>
2019-02-18 11:56:43 +0100

    BUG: Use classic ITK thread model in Source and Filter classes for ITK5
    Switched off modern ITK5 DynamicMultiThreading for ITK5, as `ThreadedGenerateData` should still be called.
    
    Fixes elastix-ITK5 test failures.
    
    See also:
    https://github.com/InsightSoftwareConsortium/ITK/blob/v5.0rc01/Documentation/ITK5MigrationGuide.md#multithreading-refactored

--- Common/itkParabolicErodeDilateImageFilter.hxx
+++ Common/itkParabolicErodeDilateImageFilter.hxx
@@ -54,6 +54,11 @@ ParabolicErodeDilateImageFilter< TInputImage, doDilate, TOutputImage >
     m_MagnitudeSign = -1;
   }
   m_UseImageSpacing = false;
+
+#if ITK_VERSION_MAJOR >= 5
+  // Use the classic (ITK4) threading model, to ensure ThreadedGenerateData is being called.
+  this->itk::ImageSource<TInputImage>::DynamicMultiThreadingOff();
+#endif
 }
 
 
