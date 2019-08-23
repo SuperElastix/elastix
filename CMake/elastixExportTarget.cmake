0586b5dd60baecf4223be1cb68969ecc2ebc9cb5
Matt McCormick <matt.mccormick@kitware.com>
2019-08-23 15:25:53 -0400

    BUG: Use elastix_SOURCE_DIR / elastix_BINARY_DIR
    Instead of CMAKE_SOURCE_DIR / CMAKE_BINARY_DIR so elastix can be used
    inside other projects with add_subdirectory.

--- CMake/elastixExportTarget.cmake
+++ CMake/elastixExportTarget.cmake
@@ -2,7 +2,7 @@ function(elastix_export_target tgt)
   # Remove the build tree's ElastixTargets file if this is the first call:
   get_property(first_time GLOBAL PROPERTY ELASTIX_FIRST_EXPORTED_TARGET)
   if(NOT first_time)
-    file(REMOVE ${CMAKE_BINARY_DIR}/ElastixTargets.cmake)
+    file(REMOVE ${elastix_BINARY_DIR}/ElastixTargets.cmake)
     set_property(GLOBAL PROPERTY ELASTIX_FIRST_EXPORTED_TARGET 1)
   endif()
 
@@ -17,6 +17,6 @@ function(elastix_export_target tgt)
   endif()
 
   export(TARGETS ${tgt}
-    APPEND FILE "${CMAKE_BINARY_DIR}/ElastixTargets.cmake"
+    APPEND FILE "${elastix_BINARY_DIR}/ElastixTargets.cmake"
   )
 endfunction()
