5ff8d976db82056658f0aa96517850a5aeba8230
Matt McCormick <matt.mccormick@kitware.com>
2020-01-30 16:44:49 -0500

    BUG: ElastixFilter, TransformixFilter itkTypeMacro arguments
    The first argument to itkTypeMacro is used to generate the output of the
    GetClassName() method -- this should be the specific class name instead
    of Self.

--- Core/Main/elxElastixFilter.h
+++ Core/Main/elxElastixFilter.h
@@ -47,7 +47,7 @@ public:
   itkNewMacro( Self );
 
   /** Run-time type information (and related methods). */
-  itkTypeMacro( Self, itk::ImageSource );
+  itkTypeMacro( ElastixFilter, itk::ImageSource );
 
   /** Typedefs. */
   typedef elastix::ElastixMain                      ElastixMainType;
