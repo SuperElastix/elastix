74ab048923763f40d2de56e9f4fff622b8435905
Niels Dekker <N.Dekker@lumc.nl>
2020-03-06 18:27:56 +0100

    ENH: Allow building exe and lib of elastix and transformix together

--- CMake/elastixExportTarget.cmake
+++ CMake/elastixExportTarget.cmake
@@ -12,8 +12,17 @@ function(elastix_export_target tgt)
       type STREQUAL "SHARED_LIBRARY")
     set_property(TARGET ${tgt} PROPERTY VERSION 1)
     set_property(TARGET ${tgt} PROPERTY SOVERSION 1)
-    set_property(TARGET ${tgt} PROPERTY
-      OUTPUT_NAME ${tgt}-${ELASTIX_VERSION_MAJOR}.${ELASTIX_VERSION_MINOR})
+
+    if ("${tgt}" STREQUAL "elastix_lib")
+      set_property(TARGET ${tgt} PROPERTY
+        OUTPUT_NAME elastix-${ELASTIX_VERSION_MAJOR}.${ELASTIX_VERSION_MINOR})
+    elseif ("${tgt}" STREQUAL "transformix_lib")
+      set_property(TARGET ${tgt} PROPERTY
+        OUTPUT_NAME transformix-${ELASTIX_VERSION_MAJOR}.${ELASTIX_VERSION_MINOR})
+    else()
+      set_property(TARGET ${tgt} PROPERTY
+        OUTPUT_NAME ${tgt}-${ELASTIX_VERSION_MAJOR}.${ELASTIX_VERSION_MINOR})
+    endif()
   endif()
 
   export(TARGETS ${tgt}
