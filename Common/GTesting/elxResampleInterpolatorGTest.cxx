4f4d43e591a92cea9b48d2e43c4653a286a21ce0
Niels Dekker <N.Dekker@lumc.nl>
2021-01-29 16:28:20 +0100

    ENH: Test Resampler, ResampleInterpolator CreateTransformParametersMap
    Added two GoogleTest unit tests:
    
     - Resampler.CreateTransformParametersMapForDefaultResampler
     - ResampleInterpolator.CreateTransformParametersMapForDefaultInterpolator
    
    Moved common code of Resampler, ResampleInterpolator, and Transform unit tests to "elxGTestUtilities.h"

---
+++ Common/GTesting/elxResampleInterpolatorGTest.cxx
@@ -0,0 +1,107 @@
+/*=========================================================================
+ *
+ *  Copyright UMC Utrecht and contributors
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
+// First include the header files to be tested:
+#include "BSplineResampleInterpolator/elxBSplineResampleInterpolator.h"
+#include "BSplineResampleInterpolatorFloat/elxBSplineResampleInterpolatorFloat.h"
+#include "LinearResampleInterpolator/elxLinearResampleInterpolator.h"
+#include "NearestNeighborResampleInterpolator/elxNearestNeighborResampleInterpolator.h"
+#include "RayCastResampleInterpolator/elxRayCastResampleInterpolator.h"
+#include "RDBSplineResampleInterpolator/elxRDBSplineResampleInterpolator.h" // For ReducedDimensionBSplineResampleInterpolator.
+
+#include "elxElastixTemplate.h"
+#include "elxGTestUtilities.h"
+
+// ITK header file:
+#include <itkImage.h>
+
+// GoogleTest header file:
+#include <gtest/gtest.h>
+
+// Standard C++ header file:
+#include <typeinfo>
+
+
+using ParameterValuesType = itk::ParameterFileParser::ParameterValuesType;
+using ParameterMapType = itk::ParameterFileParser::ParameterMapType;
+
+namespace
+{
+
+template <unsigned NDimension>
+using ElastixType = elx::ElastixTemplate<itk::Image<float, NDimension>, itk::Image<float, NDimension>>;
+
+
+// All tests specific to a dimension.
+template <unsigned NDimension>
+struct WithDimension
+{
+  template <template <typename> typename TInterpolatorTemplate>
+  struct WithInterpolator
+  {
+    using InterpolatorType = TInterpolatorTemplate<ElastixType<NDimension>>;
+
+    static void
+    Test_CreateTransformParametersMap_for_default_interpolator(const ParameterMapType & expectedDerivedParameterMap)
+    {
+      SCOPED_TRACE(std::string("Function = ")
+                     .append(__func__)
+                     .append("\n  InterpolatorType = ")
+                     .append(typeid(InterpolatorType).name()));
+
+      const auto                                                     newInterpolator = InterpolatorType::New();
+      const elx::ResampleInterpolatorBase<ElastixType<NDimension>> & interpolator = *newInterpolator;
+
+      ParameterMapType actualParameterMap;
+      interpolator.CreateTransformParametersMap(&actualParameterMap);
+
+      const ParameterMapType expectedBaseParameterMap = { { "ResampleInterpolator",
+                                                            { interpolator.elxGetClassName() } } };
+
+      elx::GTestUtilities::ExpectAllKeysUnique(expectedDerivedParameterMap, expectedBaseParameterMap);
+      EXPECT_EQ(actualParameterMap,
+                elx::GTestUtilities::MakeMergedMap(expectedDerivedParameterMap, expectedBaseParameterMap));
+    }
+  };
+
+
+  static void
+  Test_CreateTransformParametersMap_for_default_interpolator()
+  {
+    using namespace elx;
+
+    WithInterpolator<BSplineResampleInterpolator>::Test_CreateTransformParametersMap_for_default_interpolator(
+      { { "FinalBSplineInterpolationOrder", { "3" } } });
+    WithInterpolator<BSplineResampleInterpolatorFloat>::Test_CreateTransformParametersMap_for_default_interpolator({});
+    WithInterpolator<LinearResampleInterpolator>::Test_CreateTransformParametersMap_for_default_interpolator({});
+    WithInterpolator<NearestNeighborResampleInterpolator>::Test_CreateTransformParametersMap_for_default_interpolator(
+      {});
+    WithInterpolator<RayCastResampleInterpolator>::Test_CreateTransformParametersMap_for_default_interpolator({});
+    WithInterpolator<
+      ReducedDimensionBSplineResampleInterpolator>::Test_CreateTransformParametersMap_for_default_interpolator({});
+  }
+};
+
+} // namespace
+
+
+GTEST_TEST(ResampleInterpolator, CreateTransformParametersMapForDefaultInterpolator)
+{
+  WithDimension<2>::Test_CreateTransformParametersMap_for_default_interpolator();
+  WithDimension<3>::Test_CreateTransformParametersMap_for_default_interpolator();
+}
