07cdc71975aeba43506bd3e26e31f419052a4a17
Niels Dekker <N.Dekker@lumc.nl>
2022-02-20 22:00:16 +0100

    STYLE: Make BSplineTransform `m_RecursiveBSplineWeightFunction` private
    Made `RecursiveBSplineTransform` data member `m_RecursiveBSplineWeightFunction` and type `RecursiveBSplineWeightFunctionType` private, as they are only for internal use, inside `RecursiveBSplineTransform`. Hopefully addressed the comment that said:
    
        // TODO: get rid of this and use the kernels directly
    
    Which was there already in the original version of itkRecursiveBSplineTransform.h, commit 5a80b720269e7c6b660283d75bb9a087f6dcb678 "ENH: adding RecursiveBSplineTransform ... From the whuizinga branch", 9 June 2016.

--- Common/Transforms/itkRecursiveBSplineTransform.h
+++ Common/Transforms/itkRecursiveBSplineTransform.h
@@ -110,10 +110,6 @@ public:
   /** Parameter index array type. */
   using typename Superclass::ParameterIndexArrayType;
 
-  using RecursiveBSplineWeightFunctionType = typename itk::
-    RecursiveBSplineInterpolationWeightFunction<TScalarType, NDimensions, VSplineOrder>; // TODO: get rid of this and
-                                                                                         // use the kernels directly.
-
   /** Compute point transformation. This one is commonly used.
    * It calls RecursiveBSplineTransformImplementation2::InterpolateTransformPoint
    * for a recursive implementation.
@@ -181,8 +177,6 @@ protected:
   using typename Superclass::JacobianImageType;
   using typename Superclass::JacobianPixelType;
 
-  typename RecursiveBSplineWeightFunctionType::Pointer m_RecursiveBSplineWeightFunction;
-
   /** Compute the nonzero Jacobian indices. */
   void
   ComputeNonZeroJacobianIndices(NonZeroJacobianIndicesType & nonZeroJacobianIndices,
@@ -192,6 +186,11 @@ private:
   RecursiveBSplineTransform(const Self &) = delete;
   void
   operator=(const Self &) = delete;
+
+  using RecursiveBSplineWeightFunctionType =
+    itk::RecursiveBSplineInterpolationWeightFunction<TScalarType, NDimensions, VSplineOrder>;
+
+  typename RecursiveBSplineWeightFunctionType::Pointer m_RecursiveBSplineWeightFunction;
 };
 
 } // end namespace itk
