0b2c883aa3a8ecf3b287746010ae79c2c5d051d8
Niels Dekker <N.Dekker@lumc.nl>
2022-06-01 16:28:53 +0200

    STYLE: Replace stringstream with either ostringstream or istringstream
    Following CB Bailey's answer to "What's the difference between istringstream, ostringstream and stringstream?", https://stackoverflow.com/questions/3292107/whats-the-difference-between-istringstream-ostringstream-and-stringstream-w (July 20, 2010):
    
    > Using 'just' `istringstream` or `ostringstream` better expresses your intent and gives you some checking against silly mistakes such as accidental use of `<<` vs `>>`.

--- Common/ParameterFileParser/itkParameterMapInterface.h
+++ Common/ParameterFileParser/itkParameterMapInterface.h
@@ -148,7 +148,7 @@ public:
     {
       if (printThisErrorMessage && this->m_PrintErrorMessages)
       {
-        std::stringstream ss;
+        std::ostringstream ss;
         ss << "WARNING: The parameter \"" << parameterName << "\", requested at entry number " << entry_nr
            << ", does not exist at all.\n"
            << "  The default value \"" << parameterValue << "\" is used instead." << std::endl;
@@ -166,7 +166,7 @@ public:
     {
       if (printThisErrorMessage && this->m_PrintErrorMessages)
       {
-        std::stringstream ss;
+        std::ostringstream ss;
         ss << "WARNING: The parameter \"" << parameterName << "\" does not exist at entry number " << entry_nr
            << ".\n  The default value \"" << parameterValue << "\" is used instead." << std::endl;
         errorMessage = ss.str();
@@ -298,7 +298,7 @@ public:
     {
       if (printThisErrorMessage && this->m_PrintErrorMessages)
       {
-        std::stringstream ss;
+        std::ostringstream ss;
         ss << "WARNING: The parameter \"" << parameterName << "\", requested between entry numbers " << entry_nr_start
            << " and " << entry_nr_end << ", does not exist at all.\n"
            << "  The default values are used instead." << std::endl;
