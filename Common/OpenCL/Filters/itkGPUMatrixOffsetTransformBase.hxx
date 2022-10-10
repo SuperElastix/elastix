SuperElastix/elastix

commit b556886c26c0a9301d81325e6dbb9d4365c68a76

COMP: Fix OPENCL errors GetInverseMatrix() and GetOffset() (issue #647)
Addressed the compile errors reported by Georg Walther, issue https://github.com/SuperElastix/elastix/issues/647 "[GPU] Compilation fails with ELASTIX_USE_OPENCL: ON", including:

> error: ‘GetInverseMatrix()’ is private within this context
> error: ‘GPUAdvancedEuler3DTransform’ has no member named ‘GetOffset’

`GetInverseMatrix()` was used only to pass the inverse matrix to the `inverse_matrix` data members of `GPUMatrixOffsetTransformBase` (the OpenCL implementation), but these data members were never actually used, so they could simply be removed. All `GetCPUInverseMatrix()` member functions then also appeared unnecessary.

`AdvancedMatrixOffsetTransformBase::GetOffset()` needed to be restored (albeit only `protected`), as it was removed by pull request https://github.com/SuperElastix/elastix/pull/544 commit 15a3c3f07669fa811fc5896b9b01863a33c4b73b "STYLE: Remove unused AdvancedMatrixOffsetTransformBase member functions", October 29, 2021.

With help from Denis P. Shamonin.

Common/OpenCL/Filters/itkGPUMatrixOffsetTransformBase.hxx
@@ -235,7 +235,6 @@ GPUMatrixOffsetTransformBase<TScalarType, NInputDimensions, NOutputDimensions>::
       GPUMatrixOffsetTransformBase1D transformBase;
       SetMatrix1<ScalarType>(GetCPUMatrix(), transformBase.matrix, odim, idim);
       SetOffset1<ScalarType>(GetCPUOffset(), transformBase.offset, odim);
-      SetMatrix1<ScalarType>(GetCPUInverseMatrix(), transformBase.inverse_matrix, idim, odim);
       this->m_ParametersDataManager->SetCPUBufferPointer(&transformBase);
     }
     break;
@@ -244,7 +243,6 @@ GPUMatrixOffsetTransformBase<TScalarType, NInputDimensions, NOutputDimensions>::
       GPUMatrixOffsetTransformBase2D transformBase;
       SetMatrix2<ScalarType>(GetCPUMatrix(), transformBase.matrix, odim, idim);
       SetOffset2<ScalarType>(GetCPUOffset(), transformBase.offset, odim);
-      SetMatrix2<ScalarType>(GetCPUInverseMatrix(), transformBase.inverse_matrix, idim, odim);
       this->m_ParametersDataManager->SetCPUBufferPointer(&transformBase);
     }
     break;
@@ -253,7 +251,6 @@ GPUMatrixOffsetTransformBase<TScalarType, NInputDimensions, NOutputDimensions>::
       GPUMatrixOffsetTransformBase3D transformBase;
       SetMatrix3<ScalarType>(GetCPUMatrix(), transformBase.matrix, odim, idim);
       SetOffset3<ScalarType>(GetCPUOffset(), transformBase.offset, odim);
-      SetMatrix3<ScalarType>(GetCPUInverseMatrix(), transformBase.inverse_matrix, idim, odim);
       this->m_ParametersDataManager->SetCPUBufferPointer(&transformBase);
     }
     break;

Common/OpenCL/Filters/itkGPUAdvancedEuler2DTransform.h
@@ -65,13 +65,6 @@ public:
     return this->GetMatrix();
   }
 
-  /** Get CPU inverse matrix of an MatrixOffsetTransformBase. */
-  const CPUInverseMatrixType &
-  GetCPUInverseMatrix() const override
-  {
-    return this->GetInverseMatrix();
-  }
-
   /** Get CPU offset of an MatrixOffsetTransformBase. */
   const CPUOutputVectorType &
   GetCPUOffset() const override

Common/OpenCL/Filters/itkGPUAdvancedEuler3DTransform.h
@@ -65,13 +65,6 @@ public:
     return this->GetMatrix();
   }
 
-  /** Get CPU inverse matrix of an MatrixOffsetTransformBase. */
-  const CPUInverseMatrixType &
-  GetCPUInverseMatrix() const override
-  {
-    return this->GetInverseMatrix();
-  }
-
   /** Get CPU offset of an MatrixOffsetTransformBase. */
   const CPUOutputVectorType &
   GetCPUOffset() const override

Common/OpenCL/Filters/itkGPUAdvancedMatrixOffsetTransformBase.h
@@ -67,13 +67,6 @@ public:
     return this->GetMatrix();
   }
 
-  /** Get CPU inverse matrix of an MatrixOffsetTransformBase. */
-  const CPUInverseMatrixType &
-  GetCPUInverseMatrix() const override
-  {
-    return this->GetInverseMatrix();
-  }
-
   /** Get CPU offset of an MatrixOffsetTransformBase. */
   const CPUOutputVectorType &
   GetCPUOffset() const override

Common/OpenCL/Filters/itkGPUAdvancedSimilarity2DTransform.h
@@ -65,13 +65,6 @@ public:
     return this->GetMatrix();
   }
 
-  /** Get CPU inverse matrix of an MatrixOffsetTransformBase. */
-  const CPUInverseMatrixType &
-  GetCPUInverseMatrix() const override
-  {
-    return this->GetInverseMatrix();
-  }
-
   /** Get CPU offset of an MatrixOffsetTransformBase. */
   const CPUOutputVectorType &
   GetCPUOffset() const override

Common/OpenCL/Filters/itkGPUAdvancedSimilarity3DTransform.h
@@ -65,13 +65,6 @@ public:
     return this->GetMatrix();
   }
 
-  /** Get CPU inverse matrix of an MatrixOffsetTransformBase. */
-  const CPUInverseMatrixType &
-  GetCPUInverseMatrix() const override
-  {
-    return this->GetInverseMatrix();
-  }
-
   /** Get CPU offset of an MatrixOffsetTransformBase. */
   const CPUOutputVectorType &
   GetCPUOffset() const override

Common/OpenCL/Filters/itkGPUAffineTransform.h
@@ -68,13 +68,6 @@ public:
     return this->GetMatrix();
   }
 
-  /** Get CPU inverse matrix of an MatrixOffsetTransformBase. */
-  virtual const CPUInverseMatrixType &
-  GetCPUInverseMatrix() const
-  {
-    return this->GetInverseMatrix();
-  }
-
   /** Get CPU offset of an MatrixOffsetTransformBase. */
   virtual const CPUOutputVectorType &
   GetCPUOffset() const

Common/OpenCL/Filters/itkGPUEuler2DTransform.h
@@ -65,13 +65,6 @@ public:
     return this->GetMatrix();
   }
 
-  /** Get CPU inverse matrix of an MatrixOffsetTransformBase. */
-  virtual const CPUInverseMatrixType &
-  GetCPUInverseMatrix() const
-  {
-    return this->GetInverseMatrix();
-  }
-
   /** Get CPU offset of an MatrixOffsetTransformBase. */
   virtual const CPUOutputVectorType &
   GetCPUOffset() const

commit 790a46f352eb725fd54dc8bd6247535c5882c01d

BUG: Fix OpenCL resampler when using MatrixOffset transforms
The OpenCL resampler produced an empty image when using Euler, Affine,
or Similarity transforms, which all inherit from the
GPUMatrixOffsetTransformBase. All respective tests were failing.

It appears that the error was introduced by commit b556886, when fixing
issue #647. The 'inverse_matrix' data member of struct
GPUMatrixOffsetTransformBase1D/2D/3D was not removed, effecively passing
unitialized memory to the GPU.

Common/OpenCL/Filters/itkGPUMatrixOffsetTransformBase.hxx
@@ -27,21 +27,18 @@ namespace ITKGPUMatrixOffsetTransformBase
 typedef struct
 {
   cl_float matrix;
-  cl_float inverse_matrix;
   cl_float offset;
 } GPUMatrixOffsetTransformBase1D;
 
 typedef struct
 {
   cl_float4 matrix;
-  cl_float4 inverse_matrix;
   cl_float2 offset;
 } GPUMatrixOffsetTransformBase2D;
 
 typedef struct
 {
-  cl_float16 matrix;         // OpenCL does not have float9
-  cl_float16 inverse_matrix; // OpenCL does not have float9
+  cl_float16 matrix; // OpenCL does not have float9
   cl_float3  offset;
 } GPUMatrixOffsetTransformBase3D;
 