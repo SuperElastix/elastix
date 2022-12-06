68c82863d0e3f1a02e65054a9dbfcfe90f85ae46
Niels Dekker <N.Dekker@lumc.nl>
2022-12-06 15:24:10 +0100

    ENH: Add `GetNumberOfTransforms()` to itk::ElastixRegistrationMethod

--- Core/Main/GTesting/itkElastixRegistrationMethodGTest.cxx
+++ Core/Main/GTesting/itkElastixRegistrationMethodGTest.cxx
@@ -558,6 +558,36 @@ GTEST_TEST(itkElastixRegistrationMethod, InitialTransformParameterFileLinkToTran
 }
 
 
+GTEST_TEST(itkElastixRegistrationMethod, GetNumberOfTransforms)
+{
+  constexpr auto ImageDimension = 2U;
+  using ImageType = itk::Image<float, ImageDimension>;
+  const auto image =
+    CreateImageFilledWithSequenceOfNaturalNumbers<ImageType::PixelType>(itk::Size<ImageDimension>{ 5, 6 });
+
+  DefaultConstructibleElastixRegistrationMethod<ImageType, ImageType> registration;
+
+  registration.SetFixedImage(image);
+  registration.SetMovingImage(image);
+
+  for (const bool useInitialTransform : { false, true })
+  {
+    registration.SetInitialTransformParameterFileName(
+      useInitialTransform ? (GetDataDirectoryPath() + "/Translation(1,-2)/TransformParameters.txt") : "");
+
+    registration.SetParameterObject(CreateParameterObject({ // Parameters in alphabetic order:
+                                                            { "AutomaticTransformInitialization", "false" },
+                                                            { "ImageSampler", "Full" },
+                                                            { "MaximumNumberOfIterations", "0" },
+                                                            { "Metric", "AdvancedNormalizedCorrelation" },
+                                                            { "Optimizer", "AdaptiveStochasticGradientDescent" },
+                                                            { "Transform", "BSplineTransform" } }));
+    registration.Update();
+    EXPECT_EQ(registration.GetNumberOfTransforms(), useInitialTransform ? 2 : 1);
+  }
+}
+
+
 GTEST_TEST(itkElastixRegistrationMethod, WriteCompositeTransform)
 {
   constexpr auto ImageDimension = 2U;
