SuperElastix/elastix

commit c2a5de712dd14ade6f36cc45b443e8f7113b501d

STYLE: Add `MainBase`, a base class of ElastixMain and TransformixMain
Allows distinguishing between elastix and transformix specific functionality more clearly that when having ElastixMain as base class of TransformixMain.

Core/Kernel/elxElastixMain.cxx
@@ -16,24 +16,7 @@
  *
  *=========================================================================*/
 
-/** If running on a Windows-system, include "windows.h".
- *  This is to set the priority, but which does not work on cygwin.
- */
-
-#if defined(_WIN32) && !defined(__CYGWIN__)
-#  include <windows.h>
-#endif
-
 #include "elxElastixMain.h"
-#include "elxComponentLoader.h"
-
-#include "elxMacro.h"
-#include "itkPlatformMultiThreader.h"
-
-#ifdef ELASTIX_USE_OPENCL
-#  include "itkOpenCLContext.h"
-#  include "itkOpenCLSetup.h"
-#endif
 
 namespace elastix
 {
@@ -45,110 +28,10 @@ namespace elastix
 ElastixMain::ElastixMain() = default;
 
 /**
- * ****************** GetComponentDatabase *********
- */
-
-const ComponentDatabase &
-ElastixMain::GetComponentDatabase()
-{
-  // Improved thread-safety by using C++11 "magic statics".
-  static const auto componentDatabase = [] {
-    const auto componentDatabase = ComponentDatabase::New();
-    const auto componentLoader = ComponentLoader::New();
-    componentLoader->SetComponentDatabase(componentDatabase);
-
-    if (componentLoader->LoadComponents() != 0)
-    {
-      log::error("Loading components failed");
-    }
-    return componentDatabase;
-  }();
-  return *componentDatabase;
-}
-
-
-/**
  * ********************** Destructor ****************************
  */
 
-ElastixMain::~ElastixMain()
-{
-#ifdef ELASTIX_USE_OPENCL
-  itk::OpenCLContext::Pointer context = itk::OpenCLContext::GetInstance();
-  if (context->IsCreated())
-  {
-    context->Release();
-  }
-#endif
-} // end Destructor
-
-
-/**
- * *************** EnterCommandLineParameters *******************
- */
-
-void
-ElastixMain::EnterCommandLineArguments(const ArgumentMapType & argmap)
-{
-
-  /** Initialize the configuration object with the
-   * command line parameters entered by the user.
-   */
-  int dummy = this->m_Configuration->Initialize(argmap);
-  if (dummy)
-  {
-    log::error("ERROR: Something went wrong during initialization of the configuration object.");
-  }
-
-} // end EnterCommandLineParameters()
-
-
-/**
- * *************** EnterCommandLineArguments *******************
- */
-
-void
-ElastixMain::EnterCommandLineArguments(const ArgumentMapType & argmap, const ParameterMapType & inputMap)
-{
-  /** Initialize the configuration object with the
-   * command line parameters entered by the user.
-   */
-  int dummy = this->m_Configuration->Initialize(argmap, inputMap);
-  if (dummy)
-  {
-    log::error("ERROR: Something went wrong during initialization of the configuration object.");
-  }
-
-} // end EnterCommandLineArguments()
-
-
-/**
- * *************** EnterCommandLineArguments *******************
- */
-
-void
-ElastixMain::EnterCommandLineArguments(const ArgumentMapType & argmap, const std::vector<ParameterMapType> & inputMaps)
-{
-  this->m_Configurations.clear();
-  this->m_Configurations.resize(inputMaps.size());
-
-  for (size_t i = 0; i < inputMaps.size(); ++i)
-  {
-    /** Initialize the configuration object with the
-     * command line parameters entered by the user.
-     */
-    this->m_Configurations[i] = Configuration::New();
-    int dummy = this->m_Configurations[i]->Initialize(argmap, inputMaps[i]);
-    if (dummy)
-    {
-      log::error(std::ostringstream{} << "ERROR: Something went wrong during initialization of configuration object "
-                                      << i << ".");
-    }
-  }
-
-  /** Copy last configuration object to m_Configuration. */
-  this->m_Configuration = this->m_Configurations[inputMaps.size() - 1];
-} // end EnterCommandLineArguments()
+ElastixMain::~ElastixMain() = default;
 
 
 /**
@@ -314,30 +197,6 @@ ElastixMain::Run()
   /** Return a value. */
   return errorCode;
 
-} // end Run()
-
-
-/**
- * **************************** Run *****************************
- */
-
-int
-ElastixMain::Run(const ArgumentMapType & argmap)
-{
-  this->EnterCommandLineArguments(argmap);
-  return this->Run();
-} // end Run()
-
-
-/**
- * **************************** Run *****************************
- */
-
-int
-ElastixMain::Run(const ArgumentMapType & argmap, const ParameterMapType & inputMap)
-{
-  this->EnterCommandLineArguments(argmap, inputMap);
-  return this->Run();
 } // end Run()
 
 
@@ -579,192 +438,6 @@ ElastixMain::GetTotalNumberOfElastixLevels()
   return this->m_Configuration->GetTotalNumberOfElastixLevels();
 
 } // end GetTotalNumberOfElastixLevels()
-
-
-/**
- * ************************* GetElastixBase ***************************
- */
-
-ElastixBase &
-ElastixMain::GetElastixBase() const
-{
-  /** Convert ElastixAsObject to a pointer to an ElastixBase. */
-  const auto elastixBase = dynamic_cast<ElastixBase *>(this->m_Elastix.GetPointer());
-  if (elastixBase == nullptr)
-  {
-    itkExceptionMacro(<< "Probably GetElastixBase() is called before having called Run()");
-  }
-
-  return *elastixBase;
-
-} // end GetElastixBase()
-
-
-/**
- * ************************* CreateComponent ***************************
- */
-
-ElastixMain::ObjectPointer
-ElastixMain::CreateComponent(const ComponentDescriptionType & name)
-{
-  /** A pointer to the New() function. */
-  const PtrToCreator  creator = GetComponentDatabase().GetCreator(name, this->m_DBIndex);
-  const ObjectPointer component = (creator == nullptr) ? nullptr : creator();
-
-  if (component.IsNull())
-  {
-    itkExceptionMacro(<< "The following component could not be created: " << name);
-  }
-
-  return component;
-
-} // end CreateComponent()
-
-
-/**
- * *********************** CreateComponents *****************************
- */
-
-ElastixMain::ObjectContainerPointer
-ElastixMain::CreateComponents(const std::string &              key,
-                              const ComponentDescriptionType & defaultComponentName,
-                              int &                            errorcode,
-                              bool                             mandatoryComponent)
-{
-  ComponentDescriptionType componentName = defaultComponentName;
-  unsigned int             componentnr = 0;
-  ObjectContainerPointer   objectContainer = ObjectContainerType::New();
-  objectContainer->Initialize();
-
-  /** Read the component name.
-   * If the user hasn't specified any component names, use
-   * the default, and give a warning.
-   */
-  bool found = this->m_Configuration->ReadParameter(componentName, key, componentnr, true);
-
-  /** If the default equals "" (so no default), the mandatoryComponent
-   * flag is true, and not component was given by the user,
-   * then elastix quits.
-   */
-  if (!found && (defaultComponentName.empty()))
-  {
-    if (mandatoryComponent)
-    {
-      log::error(std::ostringstream{} << "ERROR: the following component has not been specified: " << key);
-      errorcode = 1;
-      return objectContainer;
-    }
-    else
-    {
-      /* Just return an empty container without nagging. */
-      errorcode = 0;
-      return objectContainer;
-    }
-  }
-
-  /** Try creating the specified component. */
-  try
-  {
-    objectContainer->CreateElementAt(componentnr) = this->CreateComponent(componentName);
-  }
-  catch (const itk::ExceptionObject & excp)
-  {
-    log::error(std::ostringstream{} << "ERROR: error occurred while creating " << key << " " << componentnr << ".\n"
-                                    << excp);
-    errorcode = 1;
-    return objectContainer;
-  }
-
-  /** Check if more than one component name is given. */
-  while (found)
-  {
-    ++componentnr;
-    found = this->m_Configuration->ReadParameter(componentName, key, componentnr, false);
-    if (found)
-    {
-      try
-      {
-        objectContainer->CreateElementAt(componentnr) = this->CreateComponent(componentName);
-      }
-      catch (const itk::ExceptionObject & excp)
-      {
-        log::error(std::ostringstream{} << "ERROR: error occurred while creating " << key << " " << componentnr << ".\n"
-                                        << excp);
-        errorcode = 1;
-        return objectContainer;
-      }
-    } // end if
-  }   // end while
-
-  return objectContainer;
-
-} // end CreateComponents()
-
-
-/**
- * *********************** SetProcessPriority *************************
- */
-
-void
-ElastixMain::SetProcessPriority() const
-{
-  /** If wanted, set the priority of this process high or below normal. */
-  std::string processPriority = this->m_Configuration->GetCommandLineArgument("-priority");
-  if (processPriority == "high")
-  {
-#if defined(_WIN32) && !defined(__CYGWIN__)
-    SetPriorityClass(GetCurrentProcess(), HIGH_PRIORITY_CLASS);
-#endif
-  }
-  else if (processPriority == "abovenormal")
-  {
-#if defined(_WIN32) && !defined(__CYGWIN__)
-    SetPriorityClass(GetCurrentProcess(), ABOVE_NORMAL_PRIORITY_CLASS);
-#endif
-  }
-  else if (processPriority == "normal")
-  {
-#if defined(_WIN32) && !defined(__CYGWIN__)
-    SetPriorityClass(GetCurrentProcess(), NORMAL_PRIORITY_CLASS);
-#endif
-  }
-  else if (processPriority == "belownormal")
-  {
-#if defined(_WIN32) && !defined(__CYGWIN__)
-    SetPriorityClass(GetCurrentProcess(), BELOW_NORMAL_PRIORITY_CLASS);
-#endif
-  }
-  else if (processPriority == "idle")
-  {
-#if defined(_WIN32) && !defined(__CYGWIN__)
-    SetPriorityClass(GetCurrentProcess(), IDLE_PRIORITY_CLASS);
-#endif
-  }
-  else if (!processPriority.empty())
-  {
-    log::warn("Unsupported -priority value. Specify one of <high, abovenormal, normal, belownormal, idle, ''>.");
-  }
-
-} // end SetProcessPriority()
-
-
-/**
- * *********************** SetMaximumNumberOfThreads *************************
- */
-
-void
-ElastixMain::SetMaximumNumberOfThreads() const
-{
-  /** Get the number of threads from the command line. */
-  std::string maximumNumberOfThreadsString = this->m_Configuration->GetCommandLineArgument("-threads");
-
-  /** If supplied, set the maximum number of threads. */
-  if (!maximumNumberOfThreadsString.empty())
-  {
-    const int maximumNumberOfThreads = atoi(maximumNumberOfThreadsString.c_str());
-    itk::MultiThreaderBase::SetGlobalMaximumNumberOfThreads(maximumNumberOfThreads);
-  }
-} // end SetMaximumNumberOfThreads()
 
 
 /**

Core/CMakeLists.txt
@@ -5,6 +5,8 @@ project(elxCore)
 # Define lists of files in the subdirectories.
 
 set(KernelFilesForExecutables
+  Kernel/elxMainBase.cxx
+  Kernel/elxMainBase.h
   Kernel/elxElastixMain.cxx
   Kernel/elxElastixMain.h
   Kernel/elxTransformixMain.cxx
@@ -183,6 +185,8 @@ add_executable(elastix_exe
   Main/elastix.h
   Main/elxMainExeUtilities.cxx
   Main/elxMainExeUtilities.h
+  Kernel/elxMainBase.cxx
+  Kernel/elxMainBase.h
   Kernel/elxElastixMain.cxx
   Kernel/elxElastixMain.h
   ${InstallFilesForExecutables}
@@ -198,6 +202,8 @@ add_library(elastix_lib
   Main/elxParameterObject.h
   Main/elastixlib.cxx
   Main/elastixlib.h
+  Kernel/elxMainBase.cxx
+  Kernel/elxMainBase.h
   Kernel/elxElastixMain.cxx
   Kernel/elxElastixMain.h
   ${InstallFilesForExecutables}
@@ -214,8 +220,8 @@ add_executable(transformix_exe
   Main/elastix.h
   Main/elxMainExeUtilities.cxx
   Main/elxMainExeUtilities.h
-  Kernel/elxElastixMain.cxx
-  Kernel/elxElastixMain.h
+  Kernel/elxMainBase.cxx
+  Kernel/elxMainBase.h
   Kernel/elxTransformixMain.cxx
   Kernel/elxTransformixMain.h
   ${InstallFilesForExecutables}
@@ -231,8 +237,8 @@ add_library(transformix_lib
   Main/elxParameterObject.h
   Main/transformixlib.cxx
   Main/transformixlib.h
-  Kernel/elxElastixMain.cxx
-  Kernel/elxElastixMain.h
+  Kernel/elxMainBase.cxx
+  Kernel/elxMainBase.h
   Kernel/elxTransformixMain.cxx
   Kernel/elxTransformixMain.h
   ${InstallFilesForExecutables}

Core/Kernel/elxElastixMain.h
@@ -18,17 +18,7 @@
 #ifndef elxElastixMain_h
 #define elxElastixMain_h
 
-#include "elxComponentDatabase.h"
-
-#include "elxElastixBase.h"
-#include "itkParameterMapInterface.h"
-
-#include <itkObject.h>
-
-// Standard C++ header files:
-#include <fstream>
-#include <iostream>
-#include <string>
+#include "elxMainBase.h"
 
 
 namespace elastix
@@ -75,14 +65,14 @@ namespace elastix
  * \ingroup Kernel
  */
 
-class ElastixMain : public itk::Object
+class ElastixMain : public MainBase
 {
 public:
   ITK_DISALLOW_COPY_AND_MOVE(ElastixMain);
 
   /** Standard itk. */
   using Self = ElastixMain;
-  using Superclass = itk::Object;
+  using Superclass = MainBase;
   using Pointer = itk::SmartPointer<Self>;
   using ConstPointer = itk::SmartPointer<const Self>;
 
@@ -90,54 +80,14 @@ public:
   itkNewMacro(Self);
 
   /** Run-time type information (and related methods). */
-  itkTypeMacro(ElastixMain, Object);
+  itkTypeMacro(ElastixMain, MainBase);
 
-  /** Typedef's.*/
-
-  /** ITK base objects. */
-  using ObjectPointer = itk::Object::Pointer;
-  using DataObjectPointer = itk::DataObject::Pointer;
-
-  /** elastix components. */
-  using ArgumentMapType = Configuration::CommandLineArgumentMapType;
-  using ConfigurationPointer = Configuration::Pointer;
-  using ObjectContainerType = ElastixBase::ObjectContainerType;
-  using DataObjectContainerType = ElastixBase::DataObjectContainerType;
-  using ObjectContainerPointer = ElastixBase::ObjectContainerPointer;
-  using DataObjectContainerPointer = ElastixBase::DataObjectContainerPointer;
-  using FlatDirectionCosinesType = ElastixBase::FlatDirectionCosinesType;
-
-  /** Typedefs for the database that holds pointers to New() functions.
-   * Those functions are used to instantiate components, such as the metric etc.
-   */
-  using ComponentDatabasePointer = ComponentDatabase::Pointer;
-  using PtrToCreator = ComponentDatabase::PtrToCreator;
-  using ComponentDescriptionType = ComponentDatabase::ComponentDescriptionType;
-  using PixelTypeDescriptionType = ComponentDatabase::PixelTypeDescriptionType;
-  using ImageDimensionType = ComponentDatabase::ImageDimensionType;
-  using DBIndexType = ComponentDatabase::IndexType;
-
-  /** Typedef that is used in the elastix dll version. */
-  using ParameterMapType = itk::ParameterMapInterface::ParameterMapType;
-
-  /** Set/Get functions for the description of the image type. */
-  itkSetMacro(FixedImagePixelType, PixelTypeDescriptionType);
-  itkSetMacro(MovingImagePixelType, PixelTypeDescriptionType);
-  itkSetMacro(FixedImageDimension, ImageDimensionType);
-  itkSetMacro(MovingImageDimension, ImageDimensionType);
-  itkGetConstMacro(FixedImagePixelType, PixelTypeDescriptionType);
-  itkGetConstMacro(MovingImagePixelType, PixelTypeDescriptionType);
-  itkGetConstMacro(FixedImageDimension, ImageDimensionType);
-  itkGetConstMacro(MovingImageDimension, ImageDimensionType);
-
-  /** Set/Get functions for the fixed and moving images
+  /** Set/Get functions for the fixed images
    * (if these are not used, elastix tries to read them from disk,
    * according to the command line parameters).
    */
   itkSetObjectMacro(FixedImageContainer, DataObjectContainerType);
-  itkSetObjectMacro(MovingImageContainer, DataObjectContainerType);
   itkGetModifiableObjectMacro(FixedImageContainer, DataObjectContainerType);
-  itkGetModifiableObjectMacro(MovingImageContainer, DataObjectContainerType);
 
   /** Set/Get functions for the fixed and moving masks
    * (if these are not used, elastix tries to read them from disk,
@@ -148,45 +98,12 @@ public:
   itkGetModifiableObjectMacro(FixedMaskContainer, DataObjectContainerType);
   itkGetModifiableObjectMacro(MovingMaskContainer, DataObjectContainerType);
 
-  /** Set/Get functions for the result images
-   * (if these are not used, elastix tries to read them from disk,
-   * according to the command line parameters).
-   */
-  itkSetObjectMacro(ResultImageContainer, DataObjectContainerType);
-  itkGetModifiableObjectMacro(ResultImageContainer, DataObjectContainerType);
-
-  itkSetObjectMacro(ResultDeformationFieldContainer, DataObjectContainerType);
-  itkGetModifiableObjectMacro(ResultDeformationFieldContainer, DataObjectContainerType);
-
-  /** Set/Get the configuration object. */
-  itkSetObjectMacro(Configuration, Configuration);
-  itkGetModifiableObjectMacro(Configuration, Configuration);
-
-  /** Functions to get pointers to the elastix components.
-   * The components are returned as Object::Pointer.
-   * Before calling this functions, call run().
-   */
-  itkGetModifiableObjectMacro(Elastix, itk::Object);
-
-  /** Convenience function that returns the elastix component as
-   * a pointer to an ElastixBase. Use only after having called run()!
-   */
-  ElastixBase &
-  GetElastixBase() const;
-
   /** Get the final transform (the result of running elastix).
    * You may pass this as an InitialTransform in an other instantiation
    * of ElastixMain.
    * Only valid after calling Run()!
    */
   itkGetModifiableObjectMacro(FinalTransform, itk::Object);
-
-  /** Set/Get the initial transform
-   * the type is itk::Object, but the pointer should actually point
-   * to an itk::Transform type (or inherited from that one).
-   */
-  itkSetObjectMacro(InitialTransform, itk::Object);
-  itkGetModifiableObjectMacro(InitialTransform, itk::Object);
 
   /** Set/Get the original fixed image direction as a flat array
    * (d11 d21 d31 d21 d22 etc ) */
@@ -210,58 +127,15 @@ public:
   unsigned int
   GetTotalNumberOfElastixLevels();
 
-  /** Returns the Index that is used in elx::ComponentDatabase. */
-  itkGetConstMacro(DBIndex, DBIndexType);
-
-  /** Enter the command line parameters, which were given by the user,
-   * if elastix.exe is used to do a registration.
-   * The Configuration object will be initialized in this way.
-   */
-  virtual void
-  EnterCommandLineArguments(const ArgumentMapType & argmap);
-
-  virtual void
-  EnterCommandLineArguments(const ArgumentMapType & argmap, const ParameterMapType & inputMap);
-
-  // Version used when elastix is used as a library.
-  virtual void
-  EnterCommandLineArguments(const ArgumentMapType & argmap, const std::vector<ParameterMapType> & inputMaps);
+  using Superclass::Run;
 
   /** Start the registration
    * run() without command line parameters; it assumes that
    * EnterCommandLineParameters has been invoked already, or that
    * m_Configuration is initialized in a different way.
    */
-  virtual int
-  Run();
-
-  /** Start the registration
-   * this version of 'run' first calls this->EnterCommandLineParameters(argc,argv)
-   * and then calls run().
-   */
-  virtual int
-  Run(const ArgumentMapType & argmap);
-
-  virtual int
-  Run(const ArgumentMapType & argmap, const ParameterMapType & inputMap);
-
-  /** Set process priority, which is read from the command line arguments.
-   * Syntax:
-   * -priority \<high, belownormal\>
-   */
-  virtual void
-  SetProcessPriority() const;
-
-  /** Set maximum number of threads, which is read from the command line arguments.
-   * Syntax:
-   * -threads \<int\>
-   */
-  virtual void
-  SetMaximumNumberOfThreads() const;
-
-  /** Function to get the ComponentDatabase. */
-  static const ComponentDatabase &
-  GetComponentDatabase();
+  int
+  Run() override;
 
   /** GetTransformParametersMap */
   virtual ParameterMapType
@@ -271,38 +145,14 @@ protected:
   ElastixMain();
   ~ElastixMain() override;
 
-  /** A pointer to elastix as an itk::object. In run() this
-   * pointer will be assigned to an ElastixTemplate<>.
-   */
-  ObjectPointer m_Elastix{ nullptr };
-
-  /** The configuration object, containing the parameters and command-line arguments. */
-  ConfigurationPointer m_Configuration{ Configuration::New() };
-
-  /** A vector of configuration objects, needed when transformix is used as library. */
-  std::vector<ConfigurationPointer> m_Configurations{};
-
-  /** Description of the ImageTypes. */
-  PixelTypeDescriptionType m_FixedImagePixelType{};
-  ImageDimensionType       m_FixedImageDimension{ 0 };
-  PixelTypeDescriptionType m_MovingImagePixelType{};
-  ImageDimensionType       m_MovingImageDimension{ 0 };
-
-  DBIndexType m_DBIndex{ 0 };
-
-  /** The images and masks. */
+  /** The fixed images and masks. */
   DataObjectContainerPointer m_FixedImageContainer{ nullptr };
-  DataObjectContainerPointer m_MovingImageContainer{ nullptr };
   DataObjectContainerPointer m_FixedMaskContainer{ nullptr };
   DataObjectContainerPointer m_MovingMaskContainer{ nullptr };
   DataObjectContainerPointer m_ResultImageContainer{ nullptr };
-  DataObjectContainerPointer m_ResultDeformationFieldContainer{ nullptr };
 
   /** A transform that is the result of registration. */
   ObjectPointer m_FinalTransform{ nullptr };
-
-  /** The initial transform. */
-  ObjectPointer m_InitialTransform{ nullptr };
 
   /** Transformation parameters map containing parameters that is the
    *  result of registration.
@@ -315,35 +165,8 @@ protected:
    * from the Configuration object and obtaining the corresponding
    * DB index from the ComponentDatabase.
    */
-  virtual int
-  InitDBIndex();
-
-  /** Create a component. Make sure InitDBIndex has been called before.
-   * The input is a string, e.g. "MattesMutualInformation".
-   */
-  virtual ObjectPointer
-  CreateComponent(const ComponentDescriptionType & name);
-
-  /** Create components. Reads from the configuration object (using the provided key)
-   * the names of the components to create and store their instantiations in the
-   * provided ObjectContainer.
-   * The errorcode remains what it was if no error occurred. Otherwise it's set to 1.
-   * The 'key' is the entry inspected in the parameter file
-   * A component named 'defaultComponentName' is used when the key is not found
-   * in the parameter file. If "" is used, no default is assumed, and an error
-   * is given when the component was not specified. If the flag mandatoryComponent
-   * is set to false, no error is given, because the component may not be needed
-   * anyway.
-   *
-   * NB: this function should never be called with:
-   * ( !mandatoryComponent && defaultComponentName != "" ) == true
-   *
-   */
-  virtual ObjectContainerPointer
-  CreateComponents(const ComponentDescriptionType & key,
-                   const ComponentDescriptionType & defaultComponentName,
-                   int &                            errorcode,
-                   bool                             mandatoryComponent = true);
+  int
+  InitDBIndex() override;
 
   /** Helper function to obtain information from images on disk. */
   void

commit 37fc08c9037b88cbe237429356ff0a46dd0ee07c

COMP: Fix OpenCL elxElastixMain.cxx compilation errors
Fixed the errors that occur when ELASTIX_USE_OPENCL is enabled, saying:

> elxElastixMain.cxx(84,10): error C2039: 'CreateOpenCLContext': is not a member of 'itk'
...
> elxElastixMain.cxx(95,26): error C3861: 'CreateOpenCLLogger': identifier not found

These errors were introduced with pull request https://github.com/SuperElastix/elastix/pull/806 commit c2a5de712dd14ade6f36cc45b443e8f7113b501d "STYLE: Add `MainBase`, a base class of ElastixMain and TransformixMain", and reported by Konstantinos Ntatsis.

Core/Kernel/elxElastixMain.cxx
@@ -18,6 +18,11 @@
 
 #include "elxElastixMain.h"
 
+#ifdef ELASTIX_USE_OPENCL
+#  include "itkOpenCLContext.h"
+#  include "itkOpenCLSetup.h"
+#endif
+
 namespace elastix
 {
 