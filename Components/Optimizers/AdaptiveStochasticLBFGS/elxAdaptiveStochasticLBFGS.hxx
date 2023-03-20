eb7550f56dba49d20b34b886aa956eb80425bfd6
Niels Dekker <N.Dekker@lumc.nl>
2023-03-20 10:03:41 +0100

    BUG: Add "ShowProgressPercentage" checks to `SampleGradients` Optimizers
    Added the missing "ShowProgressPercentage" parameter checks to `SampleGradients` member functions of Components/Optimizers.
    
    Follow-up to pull request https://github.com/SuperElastix/elastix/pull/832 commit 05d2b40f23d5b1c1438521e505e349bb54fdf7e4 "ENH: Support "ShowProgressPercentage" parameter (`false` by default)"

--- Components/Optimizers/AdaptiveStochasticLBFGS/elxAdaptiveStochasticLBFGS.hxx
+++ Components/Optimizers/AdaptiveStochasticLBFGS/elxAdaptiveStochasticLBFGS.hxx
@@ -1328,10 +1328,13 @@ AdaptiveStochasticLBFGS<TElastix>::SampleGradients(const ParametersType & mu0,
     } // end for loop over metrics
   }   // end if NewSamplesEveryIteration.
 
+  const Configuration & configuration = Deref(Superclass2::GetConfiguration());
+
   /** Prepare for progress printing. */
-  const auto progressObserver = BaseComponent::IsElastixLibrary()
-                                  ? nullptr
-                                  : ProgressCommand::CreateAndSetUpdateFrequency(this->m_NumberOfGradientMeasurements);
+  const bool showProgressPercentage = configuration.RetrieveParameterValue(false, "ShowProgressPercentage", 0, false);
+  const auto progressObserver = showProgressPercentage
+                                  ? ProgressCommand::CreateAndSetUpdateFrequency(this->m_NumberOfGradientMeasurements)
+                                  : nullptr;
   // elxout << "  Sampling gradients ..." << std::endl;
 
   /** Initialize some variables for storing gradients and their magnitudes. */
