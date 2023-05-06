2435c6a515ff94ec5b55ba3c391286249edb6aca
Niels Dekker <N.Dekker@lumc.nl>
2023-05-06 12:17:40 +0200

    STYLE: Implement logging without third party dependency (spdlog)
    Gabi Melman's spdlog is a great library, but does not seem necessary here, as elastix was only using a fraction of its functionality.
    
    This commit offers just what elastix needs (thread safety and log levels), while having less lines of code.

--- Core/Kernel/elxlog.cxx
+++ Core/Kernel/elxlog.cxx
@@ -19,67 +19,113 @@
 // Its own header
 #include "elxlog.h"
 
-// Rename the spdlog namespace, to avoid naming conflicts.
-#define spdlog elx_spdlog
-
-// The logging library from https://github.com/gabime/spdlog
-#include <spdlog/spdlog.h>
-#include <spdlog/sinks/basic_file_sink.h>
-#include <spdlog/sinks/stdout_sinks.h>
-
 #include <cassert>
-#include <memory> // For make_shared.
+#include <fstream>
+#include <iostream>
+#include <mutex>
 
 namespace elastix
 {
 namespace
 {
 
-class log_data
+class logger
 {
 public:
-  auto &
-  get_log_file_logger()
+  void
+  set(const std::string & log_filename,
+      const bool          do_log_to_file,
+      const bool          do_log_to_stdout,
+      const log::level    log_level)
   {
-    return m_loggers.log_file_logger;
-  }
-
-  auto &
-  get_stdout_logger()
-  {
-    return m_loggers.stdout_logger;
-  }
-
-  auto &
-  get_multi_logger()
-  {
-    return m_loggers.multi_logger;
+    m_data = { log_filename, do_log_to_file, do_log_to_stdout, log_level };
   }
 
   void
   reset()
   {
-    m_loggers = {};
+    m_data = {};
   }
 
-private:
-  struct loggers
+  void
+  to_file(const std::string & message)
   {
-    spdlog::logger log_file_logger{ "elastix file logger" };
-    spdlog::logger stdout_logger{ "elastix stdout logger" };
-    spdlog::logger multi_logger{ "elastix multi logger" };
+    const std::lock_guard<std::mutex> lock(m_file_mutex);
+
+    if (!m_data.log_filename.empty())
+    {
+      if (!m_data.file_stream.is_open())
+      {
+        m_data.file_stream.open(m_data.log_filename);
+      }
+      m_data.file_stream << message << std::endl;
+    }
+  }
+
+
+  void
+  to_stdout(const std::string & message)
+  {
+    const std::lock_guard<std::mutex> lock(m_stdout_mutex);
+    std::cout << message << std::endl;
+  }
+
+  void
+  to_enabled_outputs(const std::string & message)
+  {
+    if (m_data.is_logging_to_stdout)
+    {
+      to_stdout(message);
+    }
+    if (m_data.is_logging_to_file)
+    {
+      to_file(message);
+    }
+  }
+
+  auto
+  get_log_level() const
+  {
+    return m_data.level;
+  }
+
+  auto
+  is_logging_to_file() const
+  {
+    return m_data.is_logging_to_file;
+  }
+
+  auto
+  is_logging_to_any_output() const
+  {
+    return m_data.is_logging_to_file || m_data.is_logging_to_stdout;
+  }
+
+
+private:
+  // All data other than the mutexes.
+  struct data
+  {
+    std::string   log_filename{};
+    bool          is_logging_to_file{};
+    bool          is_logging_to_stdout{};
+    log::level    level{};
+    std::ofstream file_stream{};
   };
 
-  loggers m_loggers;
+  data m_data{};
+
+  std::mutex m_file_mutex{};
+  std::mutex m_stdout_mutex{};
 };
 
 
 auto &
-get_log_data()
+get_logger()
 {
-  static log_data static_log_data{};
+  static logger static_logger{};
 
-  return static_log_data;
+  return static_logger;
 }
 
 std::string
@@ -97,108 +143,36 @@ get_string_from_stream(const std::ostream & stream)
 }
 
 
-spdlog::level::level_enum
-to_spdlog_level_enum(const log::level log_level)
-{
-  switch (log_level)
-  {
-    case log::level::info:
-    {
-      return spdlog::level::info;
-    }
-    case log::level::warn:
-    {
-      return spdlog::level::warn;
-    }
-    case log::level::err:
-    {
-      return spdlog::level::err;
-    }
-    case log::level::off:
-    {
-      return spdlog::level::off;
-    }
-    default:
-    {
-      assert(!"Unsupported log level!");
-      return spdlog::level::level_enum{};
-    }
-  }
-}
-
-
 void
 setup_implementation(const std::string & log_filename,
                      const bool          do_log_to_file,
                      const bool          do_log_to_stdout,
                      const log::level    log_level)
 {
-  std::vector<spdlog::sink_ptr> log_file_sink;
-  std::vector<spdlog::sink_ptr> stdout_sink;
-  std::vector<spdlog::sink_ptr> multi_logger_sinks;
-
-  if (do_log_to_file && (log_level != log::level::off))
-  {
-    // Throws an exception when the log file cannot be opened.
-    auto sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(log_filename, true);
-    sink->set_pattern("%v");
-
-    log_file_sink = { sink };
-    multi_logger_sinks.push_back(sink);
-  }
-
-  if (do_log_to_stdout)
-  {
-    auto sink = std::make_shared<spdlog::sinks::stdout_sink_mt>();
-    sink->set_pattern("%v");
-
-    stdout_sink = { sink };
-
-    if (log_level != log::level::off)
-    {
-      multi_logger_sinks.push_back(sink);
-    }
-  }
-
-  auto & data = get_log_data();
-
-  // From here, the code is exception free, so all sinks will be set properly.
-  const auto spdlog_level = to_spdlog_level_enum(log_level);
-
-  auto & log_file_logger = data.get_log_file_logger();
-
-  log_file_logger.sinks() = std::move(log_file_sink);
-  log_file_logger.set_level(spdlog_level);
-
-  data.get_stdout_logger().sinks() = std::move(stdout_sink);
-
-  auto & multi_logger = data.get_multi_logger();
-
-  multi_logger.set_level(spdlog_level);
-  multi_logger.sinks() = std::move(multi_logger_sinks);
+  get_logger().set(log_filename, do_log_to_file, do_log_to_stdout, log_level);
 }
 
-template <spdlog::level::level_enum spdlog_level>
+template <log::level log_level>
 void
 log_to_multi_logger(const std::string & message)
 {
-  auto & logger = get_log_data().get_multi_logger();
+  auto & logger = get_logger();
 
-  if (!logger.sinks().empty())
+  if (logger.is_logging_to_any_output() && (logger.get_log_level() <= log_level))
   {
-    logger.log(spdlog_level, message);
+    logger.to_enabled_outputs(message);
   }
 }
 
-template <spdlog::level::level_enum spdlog_level>
+template <log::level log_level>
 void
 log_to_multi_logger(const std::ostream & stream)
 {
-  auto & logger = get_log_data().get_multi_logger();
+  auto & logger = get_logger();
 
-  if (!logger.sinks().empty())
+  if (logger.is_logging_to_any_output() && (logger.get_log_level() <= log_level))
   {
-    logger.log(spdlog_level, get_string_from_stream(stream));
+    logger.to_enabled_outputs(get_string_from_stream(stream));
   }
 }
 
@@ -235,79 +209,79 @@ log::guard::guard(const std::string & log_filename,
 
 log::guard::~guard()
 {
-  get_log_data().reset();
+  get_logger().reset();
 }
 
 
 void
 log::info(const std::string & message)
 {
-  log_to_multi_logger<spdlog::level::info>(message);
+  log_to_multi_logger<log::level::info>(message);
 }
 
 void
 log::warn(const std::string & message)
 {
-  log_to_multi_logger<spdlog::level::warn>(message);
+  log_to_multi_logger<log::level::warn>(message);
 }
 
 void
 log::error(const std::string & message)
 {
-  log_to_multi_logger<spdlog::level::err>(message);
+  log_to_multi_logger<log::level::err>(message);
 }
 
 
 void
 log::info(const std::ostream & stream)
 {
-  log_to_multi_logger<spdlog::level::info>(stream);
+  log_to_multi_logger<log::level::info>(stream);
 }
 
 void
 log::warn(const std::ostream & stream)
 {
-  log_to_multi_logger<spdlog::level::warn>(stream);
+  log_to_multi_logger<log::level::warn>(stream);
 }
 
 void
 log::error(const std::ostream & stream)
 {
-  log_to_multi_logger<spdlog::level::err>(stream);
+  log_to_multi_logger<log::level::err>(stream);
 }
 
 void
 log::info_to_log_file(const std::string & message)
 {
-  auto & logger = get_log_data().get_log_file_logger();
+  auto & logger = get_logger();
 
-  if (!logger.sinks().empty())
+  if (logger.is_logging_to_file() && (logger.get_log_level() == level::info))
   {
-    logger.info(message);
+    logger.to_file(message);
   }
 }
 
 void
 log::info_to_log_file(const std::ostream & stream)
 {
-  auto & logger = get_log_data().get_log_file_logger();
+  auto & logger = get_logger();
 
-  if (!logger.sinks().empty())
+  if (logger.is_logging_to_file() && (logger.get_log_level() == level::info))
   {
-    logger.info(get_string_from_stream(stream));
+    logger.to_file(get_string_from_stream(stream));
   }
 }
 
 void
 log::to_stdout(const std::string & message)
 {
-  get_log_data().get_stdout_logger().info(message);
+  get_logger().to_stdout(message);
 }
 
 void
 log::to_stdout(const std::ostream & stream)
 {
-  log::to_stdout(get_string_from_stream(stream));
+  get_logger().to_stdout(get_string_from_stream(stream));
 }
 
 } // end namespace elastix
