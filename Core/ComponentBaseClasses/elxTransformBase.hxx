SuperElastix/elastix

commit 4218961f52acff25b3907407e74a9e31d3e9fcde

STYLE: Do not use an index as "InitialTransformParametersFileName" value
Abandoned the hack that was introduced with commit f9a51b81bfb7d631b75c9ceba1c4638787f72158 (June 13, 2013)

Core/ComponentBaseClasses/elxTransformBase.hxx
@@ -358,39 +358,20 @@ TransformBase<TElastix>::ReadFromFile()
   }
   else
   {
-    // The value of "InitialTransformParametersFileName" is either an index
-    // (size_t) into the vector of configuration objects, or the file name of
-    // a transform parameters file. If it is an index, call
-    // ReadInitialTransformFromConfiguration, otherwise call ReadInitialTransformFromFile.
-
-    /** Get initial transform index number. */
-    std::istringstream to_size_t(fileName);
-    size_t             index;
-    to_size_t >> index;
-
-    if (to_size_t.eof() && !to_size_t.fail())
+    /** Check if the initial transform of this transform parameter file
+     * is not the same as this transform parameter file. Otherwise,
+     * we will have an infinite loop.
+     */
+    std::string fullFileName1 = itksys::SystemTools::CollapseFullPath(fileName);
+    std::string fullFileName2 = itksys::SystemTools::CollapseFullPath(configuration.GetParameterFileName());
+    if (fullFileName1 == fullFileName2)
     {
-      /** We can safely read the initial transform. */
-      // Retrieve configuration object from internally stored vector of configuration objects.
-      this->ReadInitialTransformFromConfiguration(this->GetElastix()->GetConfiguration(index));
+      itkExceptionMacro(<< "ERROR: The InitialTransformParametersFileName is identical to the current "
+                           "TransformParameters filename! An infinite loop is not allowed.");
     }
-    else
-    {
-      /** Check if the initial transform of this transform parameter file
-       * is not the same as this transform parameter file. Otherwise,
-       * we will have an infinite loop.
-       */
-      std::string fullFileName1 = itksys::SystemTools::CollapseFullPath(fileName);
-      std::string fullFileName2 = itksys::SystemTools::CollapseFullPath(configuration.GetParameterFileName());
-      if (fullFileName1 == fullFileName2)
-      {
-        itkExceptionMacro(<< "ERROR: The InitialTransformParametersFileName is identical to the current "
-                             "TransformParameters filename! An infinite loop is not allowed.");
-      }
 
-      /** We can safely read the initial transform. */
-      this->ReadInitialTransformFromFile(fileName.c_str());
-    }
+    /** We can safely read the initial transform. */
+    this->ReadInitialTransformFromFile(fileName.c_str());
   }
 
   /** Task 3 - Read from the configuration file how to combine the

Core/Main/elastixlib.cxx
@@ -309,13 +309,6 @@ ELASTIX::RegisterImages(ImagePointer                          fixedImage,
     /** Get the transformation parameter map. */
     this->m_TransformParametersList.push_back(elastixMain->GetTransformParametersMap());
 
-    /** Set initial transform to an index number instead of a parameter filename. */
-    if (i > 0)
-    {
-      std::ostringstream toString;
-      toString << (i - 1);
-      this->m_TransformParametersList[i]["InitialTransformParametersFileName"][0] = toString.str();
-    }
   } // end loop over registrations
 
   elx::log::info("-------------------------------------------------------------------------\n");

Core/Main/itkElastixRegistrationMethod.hxx
@@ -254,11 +254,6 @@ ElastixRegistrationMethod<TFixedImage, TMovingImage>::GenerateData()
     fixedImageOriginalDirection = elastixMain->GetOriginalFixedImageDirectionFlat();
 
     transformParameterMapVector.push_back(elastixMain->GetTransformParametersMap());
-    if (i > 0)
-    {
-      transformParameterMapVector[i]["InitialTransformParametersFileName"] =
-        ParameterValueVectorType(1, std::to_string(i - 1));
-    }
 
     // TODO: Fix elastix corrupting default pixel value parameter
     transformParameterMapVector.back()["DefaultPixelValue"] = parameterMap["DefaultPixelValue"];

Core/Main/itkTransformixFilter.hxx
@@ -244,11 +244,6 @@ TransformixFilter<TMovingImage>::GenerateData()
     transformParameterMap["MovingImageDimension"] = ParameterValueVectorType(1, std::to_string(movingImageDimension));
     transformParameterMap["ResultImagePixelType"] =
       ParameterValueVectorType(1, elx::PixelTypeToString<typename TMovingImage::PixelType>());
-
-    if (i > 0)
-    {
-      transformParameterMap["InitialTransformParametersFileName"] = ParameterValueVectorType(1, std::to_string(i - 1));
-    }
   }
 
   // Run transformix

commit ef259686da8fea3ab09ca4f9e46a03ae5c48bd46

BUG: Ignore "InitialTransformParametersFileName" when configurations > 1
When there are two or more "TransformConfigurations", the sequence of transforms is entirely specified by those configurations, and not by transform parameter files. The "InitialTransformParametersFileName" parameter should then be ignored.

Aims to fix a test failure that occurred when running itkElastixRegistrationMethodTest from
https://github.com/InsightSoftwareConsortium/ITKElastix/blob/v0.16.0/test/itkElastixRegistrationMethodTest.cxx at the current revision of the elastix main branch.

The bug appears introduced with pull request https://github.com/SuperElastix/elastix/pull/837 commit 1d75664d7d2bcac9ff9c80b11281a9bce6d86212 "ENH: Find previous configuration without interpreting filename as index" (merged on March 17, 2023).

Core/ComponentBaseClasses/elxTransformBase.hxx
@@ -348,43 +348,42 @@ TransformBase<TElastix>::ReadFromFile()
   } // end if this->m_ReadWriteTransformParameters
 
   /** Task 2 - Get the InitialTransform. */
-
-  /** Get the InitialTransformName. */
-  std::string fileName = "NoInitialTransform";
-  configuration.ReadParameter(fileName, "InitialTransformParametersFileName", 0);
-
   const ElastixBase & elastixBase = Deref(Superclass::GetElastix());
 
-  /** Call the function ReadInitialTransformFromFile. */
-  if (fileName == "NoInitialTransform")
+  if (elastixBase.GetNumberOfTransformConfigurations() > 1)
   {
-    if (elastixBase.GetNumberOfTransformConfigurations() > 1)
-    {
-      const Configuration::ConstPointer previousTransformConfiguration =
-        elastixBase.GetPreviousTransformConfiguration(configuration);
+    const Configuration::ConstPointer previousTransformConfiguration =
+      elastixBase.GetPreviousTransformConfiguration(configuration);
 
-      if (previousTransformConfiguration)
-      {
-        this->ReadInitialTransformFromConfiguration(previousTransformConfiguration);
-      }
+    if (previousTransformConfiguration)
+    {
+      this->ReadInitialTransformFromConfiguration(previousTransformConfiguration);
     }
   }
   else
   {
-    /** Check if the initial transform of this transform parameter file
-     * is not the same as this transform parameter file. Otherwise,
-     * we will have an infinite loop.
-     */
-    std::string fullFileName1 = itksys::SystemTools::CollapseFullPath(fileName);
-    std::string fullFileName2 = itksys::SystemTools::CollapseFullPath(configuration.GetParameterFileName());
-    if (fullFileName1 == fullFileName2)
-    {
-      itkExceptionMacro(<< "ERROR: The InitialTransformParametersFileName is identical to the current "
-                           "TransformParameters filename! An infinite loop is not allowed.");
-    }
+    /** Get the InitialTransformName. */
+    std::string fileName = "NoInitialTransform";
+    configuration.ReadParameter(fileName, "InitialTransformParametersFileName", 0);
 
-    /** We can safely read the initial transform. */
-    this->ReadInitialTransformFromFile(fileName.c_str());
+    /** Call the function ReadInitialTransformFromFile. */
+    if (fileName != "NoInitialTransform")
+    {
+      /** Check if the initial transform of this transform parameter file
+       * is not the same as this transform parameter file. Otherwise,
+       * we will have an infinite loop.
+       */
+      std::string fullFileName1 = itksys::SystemTools::CollapseFullPath(fileName);
+      std::string fullFileName2 = itksys::SystemTools::CollapseFullPath(configuration.GetParameterFileName());
+      if (fullFileName1 == fullFileName2)
+      {
+        itkExceptionMacro(<< "ERROR: The InitialTransformParametersFileName is identical to the current "
+                             "TransformParameters filename! An infinite loop is not allowed.");
+      }
+
+      /** We can safely read the initial transform. */
+      this->ReadInitialTransformFromFile(fileName.c_str());
+    }
   }
 
   /** Task 3 - Read from the configuration file how to combine the