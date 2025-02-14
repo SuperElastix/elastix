SuperElastix/elastix

commit 06262581b8fac735c7f9970d7d2f4e6e96f39e2c

ENH: Support `OutputTransformParameterFileFormat` parameter for TOML
Supports the parameter `OutputTransformParameterMapFileFormat"` in elastix registration parameter files. Allows specifying the file format of the output transform parameter files.

Possible values:

 - `"txt"`: Use the legacy file format, "TransformParameters.*.txt" (the default)
 - `"TOML"`: Use the TOML file format, "TransformParameters.*.toml"
 - `""` (empty string): Use the default

Core/ComponentBaseClasses/elxResampleInterpolatorBase.hxx
@@ -20,7 +20,10 @@
 #define elxResampleInterpolatorBase_hxx
 
 #include "elxResampleInterpolatorBase.h"
+#include "elxConfiguration.h"
 #include "elxConversion.h"
+
+#include "itkDeref.h"
 
 #include <cassert>
 
@@ -48,12 +51,21 @@ template <typename TElastix>
 void
 ResampleInterpolatorBase<TElastix>::WriteToFile(std::ostream & transformationParameterInfo) const
 {
+  const Configuration & configuration = itk::Deref(Superclass::GetConfiguration());
+
   ParameterMapType parameterMap;
   this->CreateTransformParameterMap(parameterMap);
 
+  const std::string parameterMapFileFormat =
+    configuration.RetrieveParameterStringValue("", "OutputTransformParameterFileFormat", 0, false);
+
+  const auto format = Conversion::StringToParameterMapStringFormat(parameterMapFileFormat);
+
   /** Write ResampleInterpolator specific things. */
-  transformationParameterInfo << ("\n// ResampleInterpolator specific\n" +
-                                  Conversion::ParameterMapToString(parameterMap));
+  transformationParameterInfo << '\n'
+                              << Conversion::ParameterMapStartOfCommentString(format)
+                              << " ResampleInterpolator specific\n " +
+                                   Conversion::ParameterMapToString(parameterMap, format);
 
 } // end WriteToFile()
 

Core/ComponentBaseClasses/elxResamplerBase.hxx
@@ -641,11 +641,20 @@ template <typename TElastix>
 void
 ResamplerBase<TElastix>::WriteToFile(std::ostream & transformationParameterInfo) const
 {
+  const Configuration & configuration = itk::Deref(Superclass::GetConfiguration());
+
   ParameterMapType parameterMap;
   Self::CreateTransformParameterMap(parameterMap);
 
+  const std::string parameterMapFileFormat =
+    configuration.RetrieveParameterStringValue("", "OutputTransformParameterFileFormat", 0, false);
+
+  const auto format = Conversion::StringToParameterMapStringFormat(parameterMapFileFormat);
+
   /** Write resampler specific things. */
-  transformationParameterInfo << ("\n// Resampler specific\n" + Conversion::ParameterMapToString(parameterMap));
+  transformationParameterInfo << '\n'
+                              << Conversion::ParameterMapStartOfCommentString(format)
+                              << " Resampler specific\n " + Conversion::ParameterMapToString(parameterMap, format);
 
 } // end WriteToFile()
 

Common/GTesting/elxConversionGTest.cxx
@@ -325,11 +325,14 @@ GTEST_TEST(Conversion, ToOptimizerParameters)
 
 GTEST_TEST(Conversion, ParameterMapToString)
 {
-  EXPECT_EQ(Conversion::ParameterMapToString({}), std::string{});
-  EXPECT_EQ(Conversion::ParameterMapToString({ { "A", {} } }), "(A)\n");
-  EXPECT_EQ(Conversion::ParameterMapToString({ { "Numbers", { "0", "1" } } }), "(Numbers 0 1)\n");
-  EXPECT_EQ(Conversion::ParameterMapToString({ { "Letters", { "a", "z" } } }), "(Letters \"a\" \"z\")\n");
-  EXPECT_EQ(Conversion::ParameterMapToString(TestExample::parameterMap), TestExample::parameterMapTextString);
+  EXPECT_EQ(Conversion::ParameterMapToString({}, elx::ParameterMapStringFormat::LegacyTxt), std::string{});
+  EXPECT_EQ(Conversion::ParameterMapToString({ { "A", {} } }, elx::ParameterMapStringFormat::LegacyTxt), "(A)\n");
+  EXPECT_EQ(Conversion::ParameterMapToString({ { "Numbers", { "0", "1" } } }, elx::ParameterMapStringFormat::LegacyTxt),
+            "(Numbers 0 1)\n");
+  EXPECT_EQ(Conversion::ParameterMapToString({ { "Letters", { "a", "z" } } }, elx::ParameterMapStringFormat::LegacyTxt),
+            "(Letters \"a\" \"z\")\n");
+  EXPECT_EQ(Conversion::ParameterMapToString(TestExample::parameterMap, elx::ParameterMapStringFormat::LegacyTxt),
+            TestExample::parameterMapTextString);
 }
 
 

Core/ComponentBaseClasses/elxTransformBase.hxx
@@ -561,7 +561,12 @@ TransformBase<TElastix>::WriteToFile(std::ostream & transformationParameterInfo,
     }
   }
 
-  transformationParameterInfo << Conversion::ParameterMapToString(parameterMap);
+  const std::string parameterMapFileFormat =
+    configuration.RetrieveParameterStringValue("", "OutputTransformParameterFileFormat", 0, false);
+
+  const auto format = Conversion::StringToParameterMapStringFormat(parameterMapFileFormat);
+
+  transformationParameterInfo << Conversion::ParameterMapToString(parameterMap, format);
 
   WriteDerivedTransformDataToFile();
 

Core/Configuration/elxConfiguration.cxx
@@ -109,7 +109,8 @@ Configuration::PrintParameterMap() const
   // Separate clearly in log-file, before and after writing the parameter map.
   log::info_to_log_file(std::ostringstream{}
                         << "\n=============== start of ParameterMap ===============\n"
-                        << Conversion::ParameterMapToString(m_ParameterMapInterface->GetParameterMap())
+                        << Conversion::ParameterMapToString(m_ParameterMapInterface->GetParameterMap(),
+                                                            ParameterMapStringFormat::LegacyTxt)
                         << "\n=============== end of ParameterMap ===============\n");
 
 } // end PrintParameterMap()

Core/Install/elxConversion.cxx
@@ -219,53 +219,103 @@ Conversion::ToOptimizerParameters(const std::vector<double> & stdVector)
  */
 
 std::string
-Conversion::ParameterMapToString(const ParameterMapType & parameterMap)
+Conversion::ParameterMapToString(const ParameterMapType & parameterMap, const ParameterMapStringFormat format)
 {
-  const auto expectedNumberOfChars = std::accumulate(
-    parameterMap.cbegin(),
-    parameterMap.cend(),
-    std::size_t{},
-    [](const std::size_t numberOfChars, const std::pair<std::string, ParameterValuesType> & parameter) {
-      return numberOfChars +
-             std::accumulate(parameter.second.cbegin(),
-                             parameter.second.cend(),
-                             // Two parentheses and a linebreak are added for each parameter.
-                             parameter.first.size() + 3,
-                             [](const std::size_t numberOfCharsPerParameter, const std::string & value) {
-                               // A space character is added for each of the values.
-                               // Plus two double-quotes, if the value is not a number.
-                               return numberOfCharsPerParameter + value.size() + (Conversion::IsNumber(value) ? 1 : 3);
-                             });
-    });
-
   std::string result;
-  result.reserve(expectedNumberOfChars);
 
-  for (const auto & parameter : parameterMap)
+  if (format == ParameterMapStringFormat::Toml)
   {
-    result.push_back('(');
-    result.append(parameter.first);
-
-    for (const auto & value : parameter.second)
-    {
-      result.push_back(' ');
-
-      if (Conversion::IsNumber(value))
+    const auto parameterValueToTomlValue = [](const std::string & parameterValue) {
+      // Use the same representation in TOML as in elastix for booleans and numbers.
+      if (parameterValue == BoolToString(false) || parameterValue == BoolToString(true) || IsNumber(parameterValue))
       {
-        result.append(value);
+        return parameterValue;
       }
       else
       {
-        result.push_back('"');
-        result.append(value);
-        result.push_back('"');
+        return '"' + parameterValue + '"';
       }
+    };
+
+    for (const auto & parameter : parameterMap)
+    {
+      result.append(parameter.first).append(" = ");
+
+      const auto & parameterValues = parameter.second;
+
+      if (parameterValues.size() == 1)
+      {
+        result.append(parameterValueToTomlValue(parameterValues.front()));
+      }
+      else
+      {
+        result.push_back('[');
+
+        if (!parameterValues.empty())
+        {
+          result.append(parameterValueToTomlValue(parameterValues.front()));
+
+          const std::size_t numberOfParameterValues = parameterValues.size();
+
+          for (std::size_t i{ 1 }; i < numberOfParameterValues; ++i)
+          {
+            result.append(", ").append(parameterValueToTomlValue(parameterValues[i]));
+          }
+        }
+        result.push_back(']');
+      }
+      result.push_back('\n');
     }
-    result.append(")\n");
+  }
+  else
+  {
+    const auto expectedNumberOfChars = std::accumulate(
+      parameterMap.cbegin(),
+      parameterMap.cend(),
+      std::size_t{},
+      [](const std::size_t numberOfChars, const std::pair<std::string, ParameterValuesType> & parameter) {
+        return numberOfChars +
+               std::accumulate(parameter.second.cbegin(),
+                               parameter.second.cend(),
+                               // Two parentheses and a linebreak are added for each parameter.
+                               parameter.first.size() + 3,
+                               [](const std::size_t numberOfCharsPerParameter, const std::string & value) {
+                                 // A space character is added for each of the values.
+                                 // Plus two double-quotes, if the value is not a number.
+                                 return numberOfCharsPerParameter + value.size() +
+                                        (Conversion::IsNumber(value) ? 1 : 3);
+                               });
+      });
+
+    result.reserve(expectedNumberOfChars);
+
+    for (const auto & parameter : parameterMap)
+    {
+      result.push_back('(');
+      result.append(parameter.first);
+
+      for (const auto & value : parameter.second)
+      {
+        result.push_back(' ');
+
+        if (Conversion::IsNumber(value))
+        {
+          result.append(value);
+        }
+        else
+        {
+          result.push_back('"');
+          result.append(value);
+          result.push_back('"');
+        }
+      }
+      result.append(")\n");
+    }
+
+    // Assert that the correct number of characters was reserved.
+    assert(result.size() == expectedNumberOfChars);
   }
 
-  // Assert that the correct number of characters was reserved.
-  assert(result.size() == expectedNumberOfChars);
   return result;
 }
 
@@ -424,6 +474,34 @@ Conversion::StringToValue(const std::string & str, itk::Object *& value)
   const bool hasSucceeded = StringToValue<void *>(str, voidPtr);
   value = static_cast<itk::Object *>(voidPtr);
   return hasSucceeded;
+}
+
+
+ParameterMapStringFormat
+Conversion::StringToParameterMapStringFormat(const std::string & str)
+{
+  if (str.empty() || str == "txt")
+  {
+    return ParameterMapStringFormat::LegacyTxt;
+  }
+  if (str == "TOML")
+  {
+    return ParameterMapStringFormat::Toml;
+  }
+  itkGenericExceptionMacro("Failed to convert to the following string to ParameterMapStringFormat: \"" << str << '"');
+}
+
+std::string
+Conversion::CreateParameterMapFileNameExtension(const ParameterMapStringFormat format)
+{
+  switch (format)
+  {
+    case ParameterMapStringFormat::LegacyTxt:
+      return ".txt";
+    case ParameterMapStringFormat::Toml:
+      return ".toml";
+  }
+  itkGenericExceptionMacro("Failed to create a file name extension for '" << static_cast<int>(format) << '\'');
 }
 
 } // end namespace elastix

Core/Install/elxConversion.h
@@ -35,6 +35,12 @@ class ITK_TEMPLATE_EXPORT OptimizerParameters;
 
 namespace elastix
 {
+enum class ParameterMapStringFormat
+{
+  LegacyTxt,
+  Toml
+};
+
 /**
  * \class Conversion
  *
@@ -68,9 +74,16 @@ public:
   static itk::OptimizerParameters<double>
   ToOptimizerParameters(const std::vector<double> &);
 
-  /** Converts the specified parameter map to a text string, according to the elastix parameter text file format. */
+  /** Converts the specified parameter map to a string, according to the specified format. */
   static std::string
-  ParameterMapToString(const ParameterMapType &);
+  ParameterMapToString(const ParameterMapType &, const ParameterMapStringFormat);
+
+  /** Resturns the character sequence to indicate the start of a comment, for the specified format. */
+  static std::string_view
+  ParameterMapStartOfCommentString(const ParameterMapStringFormat format)
+  {
+    return format == ParameterMapStringFormat::Toml ? "#" : "//";
+  }
 
   /** Convenience function overload to convert a Boolean to a text string. */
   static std::string
@@ -235,6 +248,15 @@ public:
    */
   static bool
   StringToValue(const std::string & str, itk::Object *& value);
+
+  /** Converts the specified string to the corresponding enum value. Returns ParameterMapStringFormat::LegacyTxt when
+   * the string is empty. */
+  static ParameterMapStringFormat
+  StringToParameterMapStringFormat(const std::string & str);
+
+  /** Creates a parameter map file name extension for the corresponding format. */
+  static std::string
+  CreateParameterMapFileNameExtension(const ParameterMapStringFormat format);
 };
 
 } // end namespace elastix

Core/Kernel/elxElastixTemplate.hxx
@@ -732,8 +732,13 @@ ElastixTemplate<TFixedImage, TMovingImage>::AfterRegistration()
 
   if (writeFinalTansformParameters && !outputDirectoryPath.empty())
   {
+    const std::string parameterMapFileFormat =
+      configuration.RetrieveParameterStringValue("", "OutputTransformParameterFileFormat", 0, false);
+    const auto format = Conversion::StringToParameterMapStringFormat(parameterMapFileFormat);
+
     std::ostringstream makeFileName;
-    makeFileName << outputDirectoryPath << "TransformParameters." << configuration.GetElastixLevel() << ".txt";
+    makeFileName << outputDirectoryPath << "TransformParameters." << configuration.GetElastixLevel()
+                 << Conversion::CreateParameterMapFileNameExtension(format);
     std::string FileName = makeFileName.str();
 
     /** Create a final TransformParameterFile. */

commit 8e8a82b1d368bb4c3a9fcfc1819b9eacafbbfd15

BUG: Remove accidental spaces after `specific\n`
Accidentally introduced with pull request https://github.com/SuperElastix/elastix/pull/1289 commit 06262581b8fac735c7f9970d7d2f4e6e96f39e2c "ENH: Support `OutputTransformParameterFileFormat` parameter for TOML"

Core/ComponentBaseClasses/elxResampleInterpolatorBase.hxx
@@ -64,7 +64,7 @@ ResampleInterpolatorBase<TElastix>::WriteToFile(std::ostream & transformationPar
   /** Write ResampleInterpolator specific things. */
   transformationParameterInfo << '\n'
                               << Conversion::ParameterMapStartOfCommentString(format)
-                              << " ResampleInterpolator specific\n " +
+                              << " ResampleInterpolator specific\n" +
                                    Conversion::ParameterMapToString(parameterMap, format);
 
 } // end WriteToFile()

Core/ComponentBaseClasses/elxResamplerBase.hxx
@@ -654,7 +654,7 @@ ResamplerBase<TElastix>::WriteToFile(std::ostream & transformationParameterInfo)
   /** Write resampler specific things. */
   transformationParameterInfo << '\n'
                               << Conversion::ParameterMapStartOfCommentString(format)
-                              << " Resampler specific\n " + Conversion::ParameterMapToString(parameterMap, format);
+                              << " Resampler specific\n" + Conversion::ParameterMapToString(parameterMap, format);
 
 } // end WriteToFile()
 