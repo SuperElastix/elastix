afe0304dd19d2337ed8a6dfab2ab7f2b71426d1e
Valentin Boussot <valentin.boussot@univ-rennes.fr>
2025-07-01 16:04:42 +0200

    ENH: Refactored configuration logic

--- Components/Metrics/Impact/ImpactLoss.h
+++ Components/Metrics/Impact/ImpactLoss.h
@@ -24,6 +24,13 @@
  * via the `LossFactory` for use at runtime.
  *
  * Supports both static and Jacobian-based backpropagation modes.
+ *
+ * \author V. Boussot,  Univ. Rennes, INSERM, LTSI- UMR 1099, F-35000 Rennes, France
+ * \note This work was funded by the French National Research Agency as part of the VATSop project (ANR-20-CE19-0015).
+ * \note If you use the Impact anywhere we would appreciate if you cite the following article:\n
+ * V. Boussot et al., IMPACT: A Generic Semantic Loss for Multimodal Medical Image Registration, arXiv preprint
+ * arXiv:2503.24121 (2025). https://doi.org/10.48550/arXiv.2503.24121
+ *
  */
 
 #ifndef _ImpactLoss_h
