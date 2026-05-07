6e437a50988f8ceab7d097f690dccece2e872834
Niels Dekker <N.Dekker@lumc.nl>
2026-05-07 17:35:20 +0200

    STYLE: Add GetRangeOfSamples(ThreadIdType) to AdvancedImageToImageMetric
    Eases retrieving a subrange of samples for a specified work unit. Paves the way to remove duplicate code in implementations of "Threaded" member functions.

--- Common/CostFunctions/itkAdvancedImageToImageMetric.h
+++ Common/CostFunctions/itkAdvancedImageToImageMetric.h
@@ -386,6 +386,44 @@ protected:
   MovingImageLimiterOutputType m_MovingImageMinLimit{ 0 };
   MovingImageLimiterOutputType m_MovingImageMaxLimit{ 1 };
 
+
+  /** Retrieves a subrange of the samples from its ImageSampler, for the specified work unit. */
+  auto
+  GetRangeOfSamples(const ThreadIdType workUnitID) const
+  {
+    const auto & sampleContainer = m_ImageSampler->GetOutput()->CastToSTLConstContainer();
+
+    const size_t sampleContainerSize{ sampleContainer.size() };
+    const size_t numberOfWorkUnits{ this->Superclass::GetNumberOfWorkUnits() };
+    const auto   numberOfSamplesPerWorkUnit =
+      static_cast<size_t>(std::ceil(static_cast<double>(sampleContainerSize) / static_cast<double>(numberOfWorkUnits)));
+
+    using IteratorType = decltype(sampleContainer.cbegin());
+
+    struct Range
+    {
+      IteratorType Begin{};
+      IteratorType End{};
+
+      auto
+      begin() const
+      {
+        return Begin;
+      }
+
+      auto
+      end() const
+      {
+        return End;
+      }
+    };
+
+    const auto beginOfSampleContainer = sampleContainer.cbegin();
+    return Range{ beginOfSampleContainer + std::min(numberOfSamplesPerWorkUnit * workUnitID, sampleContainerSize),
+                  beginOfSampleContainer +
+                    std::min(numberOfSamplesPerWorkUnit * (workUnitID + 1), sampleContainerSize) };
+  }
+
   /** Multi-threaded metric computation. */
 
   /** Multi-threaded version of GetValue(). */
